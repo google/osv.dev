e3d99661964721fe3113b2e4112b41765b86bbfa
Oliver Chang <ochang@google.com>
2021-04-12 14:02:11 +1000

    Improve CVE version extraction.
    - Handle versionStartExcluding and versionEndIncluding by using
      the list of actual package versions to derive "introduced" and "fixed".
    - Extract versions from description as a last resort.

--- vulnfeeds/cves/versions.go
+++ vulnfeeds/cves/versions.go
@@ -15,7 +15,9 @@
 package cves
 
 import (
+	"log"
 	"net/url"
+	"regexp"
 	"strings"
 )
 
@@ -62,6 +64,74 @@ func extractGitHubCommit(link string) *FixCommit {
 	}
 }
 
+func hasVersion(validVersions []string, version string) bool {
+	return versionIndex(validVersions, version) != -1
+}
+
+func versionIndex(validVersions []string, version string) int {
+	for i, cur := range validVersions {
+		if cur == version {
+			return i
+		}
+	}
+	return -1
+}
+
+func nextVersion(validVersions []string, version string) string {
+	idx := versionIndex(validVersions, version) + 1
+	if idx < len(validVersions) {
+		return validVersions[idx]
+	}
+
+	log.Printf("Warning: %s is not a valid version", version)
+	return ""
+}
+
+func processExtractedVersion(version string) string {
+	version = strings.Trim(version, ".")
+	// Version should contain at least a "." or a number.
+	if !strings.ContainsAny(version, ".") && !strings.ContainsAny(version, "0123456789") {
+		return ""
+	}
+
+	return version
+}
+
+func extractVersionsFromDescription(validVersions []string, description string) []AffectedVersion {
+	// Match:
+	//  - x.x.x before x.x.x
+	//  - x.x.x through x.x.x
+	//  - through x.x.x
+	//  - before x.x.x
+	pattern := regexp.MustCompile(`(?i)([\w.+\-]+)?\s+(through|before)\s+(?:version\s+)?([\w.+\-]+)`)
+	matches := pattern.FindAllStringSubmatch(description, -1)
+	if matches == nil {
+		return nil
+	}
+
+	var versions []AffectedVersion
+	for _, match := range matches {
+		// Trim periods that are part of sentences.
+		introduced := processExtractedVersion(match[1])
+		fixed := processExtractedVersion(match[3])
+		if match[2] == "through" {
+			// "Through" implies inclusive range, so the fixed version is the one that comes after.
+			fixed = nextVersion(validVersions, fixed)
+		}
+
+		if introduced == "" && fixed == "" {
+			continue
+		}
+
+		versions = append(versions, AffectedVersion{
+			Introduced: introduced,
+			Fixed:      fixed,
+		})
+	}
+
+	return versions
+}
+
 func ExtractVersionInfo(cve CVEItem, validVersions []string) VersionInfo {
 	v := VersionInfo{}
 	for _, reference := range cve.CVE.References.ReferenceData {
@@ -71,30 +141,55 @@ func ExtractVersionInfo(cve CVEItem, validVersions []string) VersionInfo {
 		}
 	}
 
+	gotVersions := false
 	for _, node := range cve.Configurations.Nodes {
 		if node.Operator != "OR" {
 			continue
 		}
 
-		// TODO: Also try to parse description as these are not always reliably set.
 		for _, match := range node.CPEMatch {
 			if !match.Vulnerable {
 				continue
 			}
 
-			if match.VersionStartIncluding != "" || match.VersionEndExcluding != "" {
-				if match.VersionStartExcluding != "" || match.VersionEndIncluding != "" {
-					// TODO: handle these by using validVersions.
-					continue
-				}
-
-				v.AffectedVersions = append(v.AffectedVersions, AffectedVersion{
-					// TODO: make sure these match validVersions.
-					Introduced: match.VersionStartIncluding,
-					Fixed:      match.VersionEndExcluding,
-				})
+			introduced := ""
+			fixed := ""
+			if match.VersionStartIncluding != "" {
+				introduced = match.VersionStartIncluding
+			} else if match.VersionStartExcluding != "" {
+				introduced = nextVersion(validVersions, match.VersionStartExcluding)
 			}
+
+			if match.VersionEndExcluding != "" {
+				fixed = match.VersionEndExcluding
+			} else if match.VersionEndIncluding != "" {
+				fixed = nextVersion(validVersions, match.VersionEndIncluding)
+			}
+
+			if introduced == "" && fixed == "" {
+				continue
+			}
+
+			if introduced != "" && !hasVersion(validVersions, introduced) {
+				log.Printf("Warning: %s is not a valid introduced version", introduced)
+			}
+
+			if fixed != "" && !hasVersion(validVersions, fixed) {
+				log.Printf("Warning: %s is not a valid fixed version", fixed)
+			}
+
+			gotVersions = true
+			v.AffectedVersions = append(v.AffectedVersions, AffectedVersion{
+				Introduced: introduced,
+				Fixed:      fixed,
+			})
 		}
 	}
+
+	if !gotVersions {
+		v.AffectedVersions = extractVersionsFromDescription(validVersions, EnglishDescription(cve.CVE))
+		log.Printf("Extracted versions from description = %+v", v.AffectedVersions)
+	}
+
 	return v
 }

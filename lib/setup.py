fe110eee20684e6f2b05a4c067fb31879e456932
Oliver Chang <ochang@google.com>
2021-09-08 13:06:56 +1000

    Add GitHub action for publishing PyPI package.

--- lib/setup.py
+++ lib/setup.py
@@ -19,7 +19,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='osv',
-    version='0.0.4',
+    version='0.0.5',
     author='OSV authors',
     author_email='osv-discuss@googlegroups.com',
     description='Open Source Vulnerabilities library',

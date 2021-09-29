a72b0f899bb35939f65c2eee4653a4534fdca938
Oliver Chang <ochang@google.com>
2021-09-29 16:38:12 +1000

    add period

--- gcp/appengine/frontend/src/components/Home.vue
+++ gcp/appengine/frontend/src/components/Home.vue
@@ -45,7 +45,7 @@
         users query whether or not their versions are impacted.
 
         These vulnerabilities are encoded in the
-        <a href="https://ossf.github.io/osv-schema/">OpenSSF Open Source Vulnerability format</a>
+        <a href="https://ossf.github.io/osv-schema/">OpenSSF Open Source Vulnerability format</a>.
       </p>
       <pre>
         curl -X POST -d \

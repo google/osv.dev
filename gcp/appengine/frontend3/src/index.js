75d2bcc66f92252246716392b20a02d3348e680c
Richard Wang <richardwa@google.com>
2022-02-02 09:39:48 +0900

    squash (#288)

--- gcp/appengine/frontend3/src/index.js
+++ gcp/appengine/frontend3/src/index.js
@@ -3,6 +3,7 @@ import '@material/mwc-icon';
 import '@material/mwc-icon-button';
 import '@material/mwc-textfield';
 import '@hotwired/turbo';
+import 'spicy-sections/src/SpicySections';
 
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));

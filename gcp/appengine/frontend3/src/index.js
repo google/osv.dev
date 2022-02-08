29d852b1576861a42ee5d010da20ae52ae6f48aa
Richard Wang <richardwa@google.com>
2022-02-08 18:42:15 +0900

    reload vuln table when ecosystem is clicked

--- gcp/appengine/frontend3/src/index.js
+++ gcp/appengine/frontend3/src/index.js
@@ -5,10 +5,38 @@ import '@material/mwc-icon-button';
 import '@hotwired/turbo';
 import 'spicy-sections/src/SpicySections';
 import {TextField as MwcTextField} from '@material/mwc-textfield';
+import {LitElement, html} from 'lit';
 
 import {MDCDataTable} from '@material/data-table';
 const dataTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
 console.log('dataTable', dataTable);
+
+// Submits a form in a way such that Turbo can intercept the event.
+// Triggering submit on the form directly would still give a correct resulting
+// page, but we want to let Turbo speed up renders as intended.
+const submitForm = function(form) {
+  if (!form) {
+    return;
+  }
+  const fakeSubmit = document.createElement('input');
+  fakeSubmit.type = 'submit';
+  fakeSubmit.style.display = 'none';
+  form.appendChild(fakeSubmit);
+  fakeSubmit.click();
+  fakeSubmit.remove();
+}
+
+// A wrapper around <input type=radio> elements that submits their parent form
+// when any radio item changes.
+export class SubmitRadiosOnClickContainer extends LitElement {
+  constructor() {
+    super();
+    this.addEventListener('change', () => submitForm(this.closest('form')))
+  }
+  // Render the contents of the element as-is.
+  render() { return html`<slot></slot>`; }
+}
+customElements.define('submit-radios', SubmitRadiosOnClickContainer);
 
 // A wrapper around <mwc-textfield> that adds back native-like enter key form
 // submission behavior.
@@ -19,18 +47,7 @@ export class MwcTextFieldWithEnter extends MwcTextField {
       if (e.key !== 'Enter') {
         return;
       }
-      const form = this.closest('form');
-      if (form) {
-        e.preventDefault();
-        // Make a fake input and submit it. If the actual form is submitted,
-        // Turbo won't intercept the event.
-        const fakeSubmit = document.createElement('input');
-        fakeSubmit.type = 'submit';
-        fakeSubmit.style.display = 'none';
-        form.appendChild(fakeSubmit);
-        fakeSubmit.click();
-        fakeSubmit.remove();
-      }
+      submitForm(this.closest('form'));
     });
   }
 }

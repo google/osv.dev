c3741f8b91375ac73a6259144a7eac4460e3c2ce
Richard Wang <richardwa@google.com>
2022-03-30 22:13:47 +0900

    fixing ecosystem counts
    it's still sorta broken but let's save the state we have today

--- gcp/appengine/frontend_handlers.py
+++ gcp/appengine/frontend_handlers.py
@@ -76,7 +76,9 @@ def index():
 
 @blueprint.route('/v2/')
 def index_v2():
-  return render_template('home.html')
+  return render_template(
+      'home.html',
+      ecosystem_counts=osv_get_ecosystem_counts())
 
 
 @blueprint.route('/v2/list')

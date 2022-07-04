c33c8675b00bd2e2c40cb6173ee8e780eaddecad
Oliver Chang <ochang@google.com>
2022-07-04 16:53:44 +1000

    URL encode generated PURLs.
    Fixes #473.

--- gcp/appengine/deploy.sh
+++ gcp/appengine/deploy.sh
@@ -4,5 +4,5 @@ pushd frontend3
 npm run build:prod
 popd
 
-pipenv lock -r > requirements.txt
+python -m pipenv lock -r > requirements.txt
 gcloud app deploy app.yaml cron.yaml cron-service.yaml --project=oss-vdb
--- lib/run_tests.sh
+++ lib/run_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 
 unset PIP_NO_BINARY
-pipenv sync
-pipenv run python -m unittest osv.bug_test
-pipenv run python -m unittest osv.ecosystems_test
-pipenv run python -m unittest osv.maven.version_test
-pipenv run python -m unittest osv.nuget_test
+python -m pipenv sync
+python -m pipenv run python -m unittest osv.bug_test
+python -m pipenv run python -m unittest osv.ecosystems_test
+python -m pipenv run python -m unittest osv.maven.version_test
+python -m pipenv run python -m unittest osv.nuget_test
+python -m pipenv run python -m unittest osv.purl_helpers_test

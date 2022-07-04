d656782696657a33a2db551cb4fc17ef23381279
Oliver Chang <ochang@google.com>
2022-07-04 16:55:48 +1000

    add missing test

---
+++ lib/osv/purl_helpers_test.py
@@ -0,0 +1,42 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Bug helper tests."""
+
+import unittest
+
+from . import purl_helpers
+
+
+class PurlHelpersTest(unittest.TestCase):
+  """purl_helpers tests."""
+
+  def test_pypi(self):
+    """Test PURL generation for PyPI."""
+    self.assertEqual('pkg:pypi/django',
+                     purl_helpers.package_to_purl('PyPI', 'django'))
+
+  def test_maven(self):
+    """Test PURL generation for Maven."""
+    self.assertEqual(
+        'pkg:maven/org.apache.struts/struts2-core',
+        purl_helpers.package_to_purl('Maven', 'org.apache.struts:struts2-core'))
+
+  def test_npm(self):
+    """Test PURL generation for npm."""
+    self.assertEqual('pkg:npm/%40hapi/hoek',
+                     purl_helpers.package_to_purl('npm', '@hapi/hoek'))
+
+
+if __name__ == '__main__':
+  unittest.main()

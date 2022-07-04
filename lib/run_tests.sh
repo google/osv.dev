de72ce55475fdb79ecaa76cc025303d87d64e521
Oliver Chang <ochang@google.com>
2022-07-04 17:00:55 +1000

    fix CI

--- lib/run_tests.sh
+++ lib/run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 unset PIP_NO_BINARY
-python -m pipenv sync
-python -m pipenv run python -m unittest osv.bug_test
-python -m pipenv run python -m unittest osv.ecosystems_test
-python -m pipenv run python -m unittest osv.maven.version_test
-python -m pipenv run python -m unittest osv.nuget_test
-python -m pipenv run python -m unittest osv.purl_helpers_test
+python3 -m pipenv sync
+python3 -m pipenv run python -m unittest osv.bug_test
+python3 -m pipenv run python -m unittest osv.ecosystems_test
+python3 -m pipenv run python -m unittest osv.maven.version_test
+python3 -m pipenv run python -m unittest osv.nuget_test
+python3 -m pipenv run python -m unittest osv.purl_helpers_test

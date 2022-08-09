google/osv.dev

commit 19b7d5f47401fed7f50b7d91c5aff89d53ded344

Homogenize  the iterator variable name to reduce cognitive load
Use the same name as the subsequent set comprehensions so it's clearer
to the reader that the same data is being operated on multiple times.

osv/models.py
@@ -338,12 +338,12 @@ class Bug(ndb.Model):
 
     search_indices.update(self._tokenize(self.id()))
 
-    for affected_package in self.affected_packages:
+    for pkg in self.affected_packages:
       # Set PURL if it wasn't provided.
-      if not affected_package.package.purl:
-        affected_package.package.purl = purl_helpers.package_to_purl(
+      if not pkg.package.purl:
+        pkg.package.purl = purl_helpers.package_to_purl(
             ecosystems.normalize(affected_package.package.ecosystem),
-            affected_package.package.name)
+            pkg.package.name)
 
     self.project = list({
         pkg.package.name for pkg in self.affected_packages if pkg.package.name

commit fae0e9bd8855d3cbd5dfa18c7922935792876a3b

Fixe issues surfaced in testing
* 19b7d5f introduced a defect, correct
* 47438b7 was passing the wrong object

osv/models.py
@@ -347,11 +347,11 @@ class Bug(ndb.Model):
     search_indices.update(self._tokenize(self.id()))
 
     for pkg in self.affected_packages:
-      pkg.package.name = self._normalize_package_name(pkg)
+      pkg.package.name = self._normalize_package_name(pkg.package)
       # Set PURL if it wasn't provided.
       if not pkg.package.purl:
         pkg.package.purl = purl_helpers.package_to_purl(
-            ecosystems.normalize(affected_package.package.ecosystem),
+            ecosystems.normalize(pkg.package.ecosystem),
             pkg.package.name)
 
     self.project = list({
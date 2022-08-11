ada259cf35d97093a63eef6fbbc2e3cb605d5b79
Rex Pan <rexpan@google.com>
2022-08-11 14:34:24 +1000

    Request chunking for scanning over 1000 packages

--- tools/osv-scanner/internal/osv/osv.go
+++ tools/osv-scanner/internal/osv/osv.go
@@ -3,12 +3,13 @@ package osv
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 const (
 	// QueryEndpoint is the URL for posting queries to OSV.
-	QueryEndpoint = "https://api-staging.osv.dev/v1/querybatch"
+	QueryEndpoint = "http://127.0.0.1:8080/v1/querybatch"
 	// BaseVulnerabilityURL is the base URL for detailed vulnerability views.
 	BaseVulnerabilityURL = "https://osv.dev/vulnerability/"
 )
@@ -57,25 +58,48 @@ func MakePURLRequest(purl string) *Query {
 	}
 }
 
+// From: https://stackoverflow.com/a/72408490
+func chunkBy[T any](items []T, chunkSize int) [][]T {
+	var _chunks = make([][]T, 0, (len(items)/chunkSize)+1)
+	for chunkSize < len(items) {
+		items, _chunks = items[chunkSize:], append(_chunks, items[0:chunkSize:chunkSize])
+	}
+	return append(_chunks, items)
+}
+
 func MakeRequest(request BatchedQuery) (*BatchedResponse, error) {
-	requestBytes, err := json.Marshal(request)
-	if err != nil {
-		return nil, err
-	}
-	requestBuf := bytes.NewBuffer(requestBytes)
 
-	resp, err := http.Post(QueryEndpoint, "application/json", requestBuf)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
+	// API has a limit of 1000 bulk query per request
+	queryChunks := chunkBy(request.Queries, 990)
+	var totalOsvResp BatchedResponse
 
-	var osvResp BatchedResponse
-	decoder := json.NewDecoder(resp.Body)
-	err = decoder.Decode(&osvResp)
-	if err != nil {
-		return nil, err
+	for _, queries := range queryChunks {
+		requestBytes, err := json.Marshal(BatchedQuery{Queries: queries})
+		if err != nil {
+			return nil, err
+		}
+		requestBuf := bytes.NewBuffer(requestBytes)
+
+		resp, err := http.Post(QueryEndpoint, "application/json", requestBuf)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			// TODO(rexpan): Better error handling
+			log.Fatalf("Server response error")
+		}
+
+		var osvResp BatchedResponse
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&osvResp)
+		if err != nil {
+			return nil, err
+		}
+
+		totalOsvResp.Results = append(totalOsvResp.Results, osvResp.Results...)
 	}
 
-	return &osvResp, nil
+	return &totalOsvResp, nil
 }

google/osv.dev

commit 19b7d5f47401fed7f50b7d91c5aff89d53ded344

Homogenize  the iterator variable name to reduce cognitive load
Use the same name as the subsequent set comprehensions so it's clearer
to the reader that the same data is being operated on multiple times.

osv/models.py
@@ -338,12 +338,12 @@ class Bug(ndb.Model):
 
     search_indices.update(self._tokenize(self.id()))
 
-    for affected_package in self.affected_packages:
+    for pkg in self.affected_packages:
       # Set PURL if it wasn't provided.
-      if not affected_package.package.purl:
-        affected_package.package.purl = purl_helpers.package_to_purl(
+      if not pkg.package.purl:
+        pkg.package.purl = purl_helpers.package_to_purl(
             ecosystems.normalize(affected_package.package.ecosystem),
-            affected_package.package.name)
+            pkg.package.name)
 
     self.project = list({
         pkg.package.name for pkg in self.affected_packages if pkg.package.name

commit 989c3a8b1d1398a95c858a1c4af0639c1f23715a

Normalize PyPI package names (#578)
* Homogenize  the iterator variable name to reduce cognitive load

Use the same name as the subsequent set comprehensions so it's clearer
to the reader that the same data is being operated on multiple times.

* Normalize PyPI package names to prevent duplicate vulnerability findings

* Refactor PyPI package normalization based on initial reviewer feedback

* Standardize quoting
* Abstract ecosystem-specific logic into normalization function

See https://github.com/google/osv.dev/pull/578 for details

* Fixe issues surfaced in testing

* 19b7d5f introduced a defect, correct
* 47438b7 was passing the wrong object

* Reformat to please lint and format action

* Move PyPI package normalization to where the bug gets updated

This will allow for the normalized value to be stored in Datastore and
also set in the pubsub message.

* Add missing comma omitted from 8d71f25

* Move PyPI package name normalization to worker

This way the package name gets normalized before it goes to Datastore
and PubSub

* Add tests for PyPI package name normalization

* Reformat recently changes with yapf

* Reformat recent changes with yapf3

docker/worker/worker.py
@@ -19,6 +19,7 @@ import json
 import logging
 import os
 import math
+import re
 import redis
 import resource
 import shutil
@@ -298,6 +299,17 @@ def fix_invalid_ghsa(vulnerability):
   return True
 
 
+def maybe_normalize_package_names(vulnerability):
+  """Normalize package names as necessary."""
+  for affected in vulnerability.affected:
+    if affected.package.ecosystem == 'PyPI':
+      # per https://peps.python.org/pep-0503/#normalized-names
+      affected.package.name = re.sub(r'[-_.]+', '-',
+                                     affected.package.name).lower()
+
+  return vulnerability
+
+
 class TaskRunner:
   """Task runner."""
 
@@ -453,6 +465,7 @@ class TaskRunner:
                  original_sha256):
     """Process updates on a vulnerability."""
     logging.info('Processing update for vulnerability %s', vulnerability.id)
+    vulnerability = maybe_normalize_package_names(vulnerability)
     if source_repo.name == 'ghsa' and not fix_invalid_ghsa(vulnerability):
       logging.warning('%s has an encoding error, skipping.', vulnerability.id)
       return

osv/models.py
@@ -338,12 +338,11 @@ class Bug(ndb.Model):
 
     search_indices.update(self._tokenize(self.id()))
 
-    for affected_package in self.affected_packages:
+    for pkg in self.affected_packages:
       # Set PURL if it wasn't provided.
-      if not affected_package.package.purl:
-        affected_package.package.purl = purl_helpers.package_to_purl(
-            ecosystems.normalize(affected_package.package.ecosystem),
-            affected_package.package.name)
+      if not pkg.package.purl:
+        pkg.package.purl = purl_helpers.package_to_purl(
+            ecosystems.normalize(pkg.package.ecosystem), pkg.package.name)
 
     self.project = list({
         pkg.package.name for pkg in self.affected_packages if pkg.package.name

docker/worker/testdata/PYSEC-456.yaml
@@ -1,0 +1,22 @@
+id: PYSEC-456
+summary: A vulnerability in an unnormalized package
+details: |
+  Blah blah blah
+  Blah
+references:
+- type: WEB
+  url: https://ref.com/ref
+affected:
+- package:
+    name: Scrapy
+    ecosystem: PyPI
+  ranges:
+  - type: ECOSYSTEM
+    events:
+    - introduced: 1.14.2
+    - fixed: 1.31.0
+  - type: GIT
+    repo: https://osv-test/repo/url
+    events:
+    - introduced: eefe8ec3f1f90d0e684890e810f3f21e8500a4cd
+    - fixed: 8d8242f545e9cec3e6d0d2e3f5bde8be1c659735

docker/worker/worker_test.py
@@ -931,6 +931,47 @@ class UpdateTest(unittest.TestCase, tests.ExpectationTest(TEST_DATA_DIR)):
 
     self.expect_equal('pypi_pubsub_calls', self.mock_publish.mock_calls)
 
+  def test_normalize_pypi(self):
+    """Test a PyPI entry normalizes as expected."""
+    self.source_repo.ignore_git = False
+    self.source_repo.versions_from_repo = False
+    self.source_repo.detect_cherrypicks = False
+    self.source_repo.put()
+
+    self.mock_repo.add_file(
+        'PYSEC-456.yaml',
+        self._load_test_data(os.path.join(TEST_DATA_DIR, 'PYSEC-456.yaml')))
+    self.mock_repo.commit('User', 'user@email')
+    task_runner = worker.TaskRunner(ndb_client, None, self.tmp_dir.name, None,
+                                    None)
+    message = mock.Mock()
+    message.attributes = {
+        'source': 'source',
+        'path': 'PYSEC-456.yaml',
+        'original_sha256': _sha256('PYSEC-456.yaml'),
+        'deleted': 'false',
+    }
+    task_runner._source_update(message)
+
+    repo = pygit2.Repository(self.remote_source_repo_path)
+    commit = repo.head.peel()
+    diff = repo.diff(commit.parents[0], commit)
+
+    self.expect_equal('diff_normalized_pypi', diff.patch)
+
+    self.expect_dict_equal(
+        'normalized_pypi',
+        ndb.Key(osv.Bug, 'source:PYSEC-456').get()._to_dict())
+
+    affected_commits = list(osv.AffectedCommit.query())
+    self.assertCountEqual([
+        'b1c95a196f22d06fcf80df8c6691cd113d8fefff',
+        'eefe8ec3f1f90d0e684890e810f3f21e8500a4cd',
+    ], [a.commit for a in affected_commits])
+
+    self.expect_equal('normalized_pypi_pubsub_calls',
+                      self.mock_publish.mock_calls)
+
   def test_update_maven(self):
     """Test updating maven."""
     self.source_repo.ignore_git = False
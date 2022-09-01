af57baef43d84fea3171482c6fc5dd9d99dfe89e
Andrew Pollock <apollock@google.com>
2022-09-01 13:46:05 +1000

    Run `go vet` on all of our go modules
    Partially fixes: #633

--- tools/lint_and_format.sh
+++ tools/lint_and_format.sh
@@ -15,6 +15,12 @@
 
 script_dir=$(dirname "$0")
 IN_SCOPE_PYTHON_FILES="$(git ls-files | grep '\.py$' | grep -v -E '(_pb2|third_party)')"
+# Explicitly excluding the docs directory due to
+# tools.go:4:2: import "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2" is a program,not an importable package
+# tools.go:5:2: import "google.golang.org/grpc/cmd/protoc-gen-go-grpc" is a program, not an importable package
+# tools.go:6:2: import "google.golang.org/protobuf/cmd/protoc-gen-go" is a program, not an importable package
+# See also https://github.com/google/osv.dev/issues/573
+IN_SCOPE_GO_MODULES="$(git ls-files | fgrep go.mod | fgrep -v docs | xargs dirname)"
 
 python_lint_findings=""
 if ! echo "$IN_SCOPE_PYTHON_FILES" | xargs pylint --rcfile="$script_dir/../.pylintrc"; then
@@ -22,11 +28,20 @@ if ! echo "$IN_SCOPE_PYTHON_FILES" | xargs pylint --rcfile="$script_dir/../.pyli
 fi
 
 python_format_findings=""
-if ! echo "$IN_PYTHON_SCOPE_FILES" | xargs yapf -d --style "$script_dir/../.style.yapf"; then
+if ! echo "$IN_SCOPE_PYTHON_FILES" | xargs yapf -d --style "$script_dir/../.style.yapf"; then
   python_format_findings="python_format_findings"
 fi
 
-if [ $python_lint_findings ] || [ $python_format_findings ]; then
+go_vet_findings=""
+for module_dir in $IN_SCOPE_GO_MODULES; do
+  pushd "$module_dir" >/dev/null
+  if ! go vet ./...; then
+    go_vet_findings="go_vet_findings"
+  fi
+  popd >/dev/null
+done
+
+if [ $python_lint_findings ] || [ $python_format_findings ] || [ $go_vet_findings ]; then
   echo "Please fix the above findings"
   exit 1
 fi

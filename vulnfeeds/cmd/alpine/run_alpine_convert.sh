00bd1de8162576da014886e0c4de93b9c65e5a4b
Rex Pan <rexpan@google.com>
2022-09-05 14:50:47 +1000

    Update scripts to follow style guide

--- vulnfeeds/cmd/alpine/run_alpine_convert.sh
+++ vulnfeeds/cmd/alpine/run_alpine_convert.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 
-OSV_PARTS_OUTPUT=parts/alpine_output
+set -e
+
+OSV_PARTS_OUTPUT="parts/alpine_output"
 
 echo "Setup initial directories"
 rm -rf $OSV_PARTS_OUTPUT && mkdir -p $OSV_PARTS_OUTPUT
 
 go run ./cmd/alpine/
 echo "Begin Syncing with cloud"
-gsutil -q -m rsync -d $OSV_PARTS_OUTPUT gs://cve-osv-conversion/parts/alpine
+gsutil -q -m rsync -d $OSV_PARTS_OUTPUT "gs://cve-osv-conversion/parts/alpine"
 echo "Successfully synced with cloud"
--- vulnfeeds/cmd/combine-to-osv/run_combine_to_osv_convert.sh
+++ vulnfeeds/cmd/combine-to-osv/run_combine_to_osv_convert.sh
@@ -1,19 +1,21 @@
-#!/bin/bash -e
+#!/bin/bash
 
-OSV_PARTS_ROOT=parts/
-OSV_OUTPUT=osv_output/
+set -e
+
+OSV_PARTS_ROOT="parts/"
+OSV_OUTPUT="osv_output/"
 
 echo "Setup initial directories"
 rm -rf $OSV_PARTS_ROOT && mkdir -p $OSV_PARTS_ROOT
 rm -rf $OSV_OUTPUT && mkdir -p $OSV_OUTPUT
 
 echo "Begin syncing with cloud parts"
-gsutil -q -m rsync -r gs://cve-osv-conversion/parts/ $OSV_PARTS_ROOT
+gsutil -q -m rsync -r "gs://cve-osv-conversion/parts/" $OSV_PARTS_ROOT
 echo "Successfully synced with cloud parts"
 
 echo "Run combine-to-osv"
 go run ./cmd/combine-to-osv/
 
 echo "Begin syncing output with cloud"
-gsutil -q -m rsync $OSV_OUTPUT gs://cve-osv-conversion/osv-output/
+gsutil -q -m rsync $OSV_OUTPUT "gs://cve-osv-conversion/osv-output/"
 echo "Successfully synced with cloud"

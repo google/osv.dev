47dee6240e213cb6286e0baf35627baed931a583
Oliver Chang <ochang@google.com>
2022-09-21 10:44:43 +1000

    safe_mode

--- gcp/appengine/frontend_handlers.py
+++ gcp/appengine/frontend_handlers.py
@@ -403,8 +403,8 @@ def group_versions(versions):
 def markdown(text):
   """Render markdown."""
   if text:
-    text = text.replace('<', '&lt;').replace('>', '&gt;')
-    return markdown2.markdown(text, extras=['fenced-code-blocks'])
+    return markdown2.markdown(
+        text, safe_mode='escape', extras=['fenced-code-blocks'])
 
   return ''
 

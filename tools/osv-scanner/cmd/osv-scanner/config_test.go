048fde20b5bc2b039e6abf52c66955a6d7a16730
Rex P <rexpan@google.com>
2022-11-07 13:55:23 +1100

    Additional tests

--- tools/osv-scanner/cmd/osv-scanner/config_test.go
+++ tools/osv-scanner/cmd/osv-scanner/config_test.go
@@ -7,20 +7,46 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+type testStruct struct {
+	targetPath string
+	configPath string
+	configErr  error
+}
+
 func TestTryLoadConfig(t *testing.T) {
 	configMap := map[string]Config{}
-	testPaths := []string{
-		"../../testdata/innerFolder/test.yaml",
-		"../../testdata/innerFolder/",
-		"../../testdata/",
+	testPaths := []testStruct{
+		{
+			targetPath: "../../testdata/testdatainner/innerFolder/test.yaml",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+		{
+			targetPath: "../../testdata/testdatainner/innerFolder/",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+		{ // Test no slash at the end
+			targetPath: "../../testdata/testdatainner/innerFolder",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+		{
+			targetPath: "../../testdata/testdatainner/",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
 	}
 
-	for _, path := range testPaths {
-		absPath, err := filepath.Abs(path)
+	for _, testPath := range testPaths {
+		absPath, err := filepath.Abs(testPath.targetPath)
 		if err != nil {
 			t.Errorf("%s", err)
 		}
-		TryLoadConfig(absPath, configMap)
+		configPath, configErr := TryLoadConfig(absPath, configMap)
+		cmp.Equal(configPath, testPath.configPath)
+		cmp.Equal(configErr, testPath.configErr)
+
 	}
 
 	for _, elem := range configMap {
@@ -29,6 +55,50 @@ func TestTryLoadConfig(t *testing.T) {
 				"GO-2022-0968",
 				"GO-2022-1059",
 			},
+		})
+	}
+
+}
+
+func TestTryLoadConfigFail(t *testing.T) {
+	configMap := map[string]Config{}
+	testPaths := []testStruct{
+		{
+			targetPath: "../../testdata/testdatainner/",
+			configPath: "",
+			configErr:  LoadConfigError{},
+		},
+		{
+			targetPath: "../../testdata/testdatainner/innerFolder/",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+		{ // Test no slash at the end
+			targetPath: "../../testdata/testdatainner/innerFolder",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+		{
+			targetPath: "../../testdata/testdatainner/",
+			configPath: "../../testdata/testdatainner/osv-scanner.toml",
+			configErr:  nil,
+		},
+	}
+
+	for _, testPath := range testPaths {
+		absPath, err := filepath.Abs(testPath.targetPath)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+		configPath, configErr := TryLoadConfig(absPath, configMap)
+		cmp.Equal(configPath, testPath.configPath)
+		cmp.Equal(configErr, testPath.configErr)
+
+	}
+
+	for _, elem := range configMap {
+		cmp.Equal(elem, Config{
+			IgnoredVulnIds: []string{},
 		})
 	}
 

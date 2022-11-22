46999670fbc51ecfcbf13d9fc56f7d113fe467ce
Andrew Pollock <apollock@google.com>
2022-11-22 12:23:36 +1100

    Return early and avoid indenting

--- vulnfeeds/cpp/main.go
+++ vulnfeeds/cpp/main.go
@@ -188,25 +188,25 @@ func CVEToOSV(CVE cves.CVEItem, repo, directory string) {
 
 	if len(v.Affected[0].Ranges) == 0 {
 		Logger.Infof("No affected versions detected for %s for %q", CVE.CVE.CVEDataMeta.ID, CPE.Product)
-	} else {
-
-		vulnDir := filepath.Join(directory, CPE.Product)
-		err = os.MkdirAll(vulnDir, 0755)
-		if err != nil {
-			Logger.Fatalf("Failed to create dir: %v", err)
-		}
-		outputFile := filepath.Join(vulnDir, v.ID+extension)
-		f, err := os.Create(outputFile)
-		if err != nil {
-			Logger.Fatalf("Failed to open %s for writing: %v", outputFile, err)
-		}
-		defer f.Close()
-		err = v.ToJSON(f)
-		if err != nil {
-			Logger.Fatalf("Failed to write %s: %v", outputFile, err)
-		}
-		Logger.Infof("Processed %s for %q", CVE.CVE.CVEDataMeta.ID, CPE.Product)
+		return
 	}
+
+	vulnDir := filepath.Join(directory, CPE.Product)
+	err = os.MkdirAll(vulnDir, 0755)
+	if err != nil {
+		Logger.Fatalf("Failed to create dir: %v", err)
+	}
+	outputFile := filepath.Join(vulnDir, v.ID+extension)
+	f, err := os.Create(outputFile)
+	if err != nil {
+		Logger.Fatalf("Failed to open %s for writing: %v", outputFile, err)
+	}
+	defer f.Close()
+	err = v.ToJSON(f)
+	if err != nil {
+		Logger.Fatalf("Failed to write %s: %v", outputFile, err)
+	}
+	Logger.Infof("Processed %s for %q", CVE.CVE.CVEDataMeta.ID, CPE.Product)
 }
 
 func main() {

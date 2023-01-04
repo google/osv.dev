df236f4b8d107be72408c32ea00388792918d65c
Andrew Pollock <apollock@google.com>
2023-01-04 12:13:15 +1100

    Turns out the new test package was more vulnerable than thought

--- gcp/api/integration_tests.py
+++ gcp/api/integration_tests.py
@@ -233,8 +233,14 @@ class IntegrationTests(unittest.TestCase):
     package = 'github.com/gin-gonic/gin'
     ecosystem = 'Go'
     go_2020_0001 = self._get('GO-2020-0001')
+    go_2021_0052 = self._get('GO-2021-0052')
     ghsa_6vm3_jj99_7229 = self._get('GHSA-6vm3-jj99-7229')
-    expected_vulns = [go_2020_0001, ghsa_6vm3_jj99_7229]
+    ghsa_h395_qcrw_5vmq = self._get('GHSA-h395-qcrw-5vmq')
+    expected_vulns = [ghsa_6vm3_jj99_7229,
+                      go_2020_0001,
+                      ghsa_h395_qcrw_5vmq,
+                      go_2021_0052,
+                      ]
 
     # Test that a SemVer (believed to be vulnerable) version and an ecosystem
     # returns expected vulnerabilities.

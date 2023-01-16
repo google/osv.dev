b06550f6db9bdb5200125117f794f130b098853a
Andrew Pollock <apollock@google.com>
2023-01-16 16:10:24 +1100

    Use a Cloud Secret for the deps.dev key
    Turns out kubectl isn't available in the ci Docker image so replicate
    the deps.dev API key as a Cloud Secret and retrieve it that way.
    
    Make the test execution more verbose so it's very clear which ones are
    being skipped and add a failsafe for the Secret not being successfully
    retrieved.

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,12 @@
 
 export PIPENV_IGNORE_VIRTUALENVS=1
 set +x  # Keep the API key out of execution logs
-# Temporarily confirm that we're getting expected output
-kubectl get secret secrets
-export DEPSDEV_API_KEY=$(kubectl get secret secrets -o jsonpath='{.data.deps\.dev}' | base64 --decode)
+export DEPSDEV_API_KEY="$(gcloud --project oss-vdb secrets versions access latest --secret depsdotdev-key)"
+test -z "$DEPSDEV_API_KEY" && echo "No API key for deps.dev" && exit 1
 set -x
 python3 -m pipenv sync
 python3 -m pipenv run python -m unittest osv.bug_test
-python3 -m pipenv run python -m unittest osv.ecosystems_test
+python3 -m pipenv run python -m unittest -v osv.ecosystems_test
 python3 -m pipenv run python -m unittest osv.maven.version_test
 python3 -m pipenv run python -m unittest osv.nuget_test
 python3 -m pipenv run python -m unittest osv.purl_helpers_test

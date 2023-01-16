2399f23c90437ed12fe960358a9d5ec1769214ca
Rex Pan <rexpan@google.com>
2023-01-17 10:53:06 +1300

    Revert "Breaks terraform format for tests"
    This reverts commit 28bce9a5300650914a32ee173ce7633090f80d7f.

--- deployment/terraform/environments/oss-vdb-test/main.tf
+++ deployment/terraform/environments/oss-vdb-test/main.tf
@@ -4,7 +4,7 @@ module "osv_test" {
   project_id = "oss-vdb-test"
 
   public_import_logs_bucket     = "osv-test-public-import-logs"
-  vulnerabilities_export_bucket   = "osv-test-vulnerabilities"
+  vulnerabilities_export_bucket = "osv-test-vulnerabilities"
 
   api_url               = "api.test.osv.dev"
   api_backend_image_tag = "20230105"

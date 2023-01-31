89abc4aa5fa8250de8c9aaf533e3ccc4aabf7677
Michael Kedar <michaelkedar@google.com>
2023-01-31 01:23:53 +0000

    SemVer ecosystem helper docstring change

--- osv/ecosystems/semver.py
+++ osv/ecosystems/semver.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""SemVer ecosystem helper."""
+"""Ecosystem helper for ecosystems using SemVer."""
 
 from .helper_base import Ecosystem
 from .. import semver_index

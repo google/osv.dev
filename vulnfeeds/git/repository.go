732f02fe1f04917f5c44182e241ab2a2b332990d
Andrew Pollock <andrewpollock@users.noreply.github.com>
2023-02-27 15:29:56 +1000

    Populate a per-repository map of normalized version tags (#1057)
    This will facilitate exact and fuzzy matches on CPE versions in CVE
    records.

--- vulnfeeds/git/repository.go
+++ vulnfeeds/git/repository.go
@@ -8,31 +8,46 @@ import (
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/go-git/go-git/v5/storage/memory"
+
+	"golang.org/x/exp/maps"
+
+	"github.com/google/osv/vulnfeeds/cves"
 )
 
-// A version holds a tag and corresponding commit hash.
-type Version struct {
+// A GitTag holds a Git tag and corresponding commit hash.
+type Tag struct {
 	Tag    string // Git tag
 	Commit string // Git commit hash
 }
 
-// Versions is an ordered array of Version.
-type Versions []Version
+type Tags []Tag
 
-func (v Versions) Len() int           { return len(v) }
-func (v Versions) Less(i, j int) bool { return v[i].Tag < v[j].Tag }
-func (v Versions) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
+func (t Tags) Len() int           { return len(t) }
+func (t Tags) Less(i, j int) bool { return t[i].Tag < t[j].Tag }
+func (t Tags) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
 
-// RepoTagsMap acts as a cache for RepoTags results, keyed on the repo's URL.
-type RepoTagsMap map[string]Versions
+// NormalizedTag holds a normalized (as by NormalizeRepoTags) tag and corresponding commit hash.
+type NormalizedTag struct {
+	OriginalTag string
+	Commit      string
+}
 
-// RepoTags returns an array of Versions being the tags and associated commits in repoURL.
+// RepoTagsMap holds all of the tags (naturally occurring and normalized) for a Git repo.
+type RepoTagsMap struct {
+	Tag           map[string]Tag           // The key is the original tag as seen on the repo.
+	NormalizedTag map[string]NormalizedTag // The key is the normalized (as by NormalizeRepoTags) original tag.
+}
+
+// RepoTags acts as a cache for RepoTags results, keyed on the repo's URL.
+type RepoTagsCache map[string]RepoTagsMap
+
+// RepoTags returns an array of Tag being the tags and associated commits in repoURL.
 // An optional repoTagsCache can be supplied to reduce repeated remote connections to the same repo.
-func RepoTags(repoURL string, repoTagsCache *RepoTagsMap) (versions Versions, e error) {
+func RepoTags(repoURL string, repoTagsCache RepoTagsCache) (tags Tags, e error) {
 	if repoTagsCache != nil {
-		versions, ok := (*repoTagsCache)[repoURL]
+		tags, ok := repoTagsCache[repoURL]
 		if ok {
-			return versions, nil
+			return maps.Values(tags.Tag), nil
 		}
 	}
 	// Cache miss.
@@ -45,22 +60,53 @@ func RepoTags(repoURL string, repoTagsCache *RepoTagsMap) (versions Versions, e 
 	repo := git.NewRemote(memory.NewStorage(), remoteConfig)
 	refs, err := repo.List(&git.ListOptions{})
 	if err != nil {
-		return versions, err
+		return tags, err
 	}
+	tagsMap := make(map[string]Tag)
 	for _, ref := range refs {
 		if !ref.Name().IsTag() {
 			continue
 		}
-		v := Version{Tag: ref.Name().Short(), Commit: ref.Hash().String()}
-		versions = append(versions, v)
+		tags = append(tags, Tag{Tag: ref.Name().Short(), Commit: ref.Hash().String()})
+		tagsMap[ref.Name().Short()] = Tag{Tag: ref.Name().Short(), Commit: ref.Hash().String()}
 	}
 	// Sort so that we get consistently ordered output for test validation purposes.
-	sort.Sort(versions)
+	sort.Sort(tags)
 	if repoTagsCache != nil {
-		*repoTagsCache = make(map[string]Versions)
-		(*repoTagsCache)[repoURL] = versions
+		repoTagsCache[repoURL] = RepoTagsMap{Tag: tagsMap, NormalizedTag: nil}
 	}
-	return versions, nil
+	return tags, nil
+}
+
+// NormalizeRepoTags returns a map of normalized tags mapping back to original tags and also commit hashes.
+// An optional repoTagsCache can be supplied to reduce repeated remote connections to the same repo.
+func NormalizeRepoTags(repoURL string, repoTagsCache RepoTagsCache) (NormalizedTags map[string]NormalizedTag, e error) {
+	if repoTagsCache != nil {
+		tags, ok := repoTagsCache[repoURL]
+		if ok && tags.NormalizedTag != nil {
+			return tags.NormalizedTag, nil
+		}
+	}
+	// Cache miss.
+	tags, err := RepoTags(repoURL, repoTagsCache)
+	if err != nil {
+		return nil, err
+	}
+	NormalizedTags = make(map[string]NormalizedTag)
+	for _, t := range tags {
+		normalizedTag, err := cves.Normalize(t.Tag)
+		if err != nil {
+			// It's conceivable that not all tags are normalizable or potentially versions.
+			continue
+		}
+		NormalizedTags[normalizedTag] = NormalizedTag{OriginalTag: t.Tag, Commit: t.Commit}
+	}
+	if repoTagsCache != nil {
+		// The RepoTags() call above will have cached the Tag map already
+		tagsMap := repoTagsCache[repoURL].Tag
+		repoTagsCache[repoURL] = RepoTagsMap{Tag: tagsMap, NormalizedTag: NormalizedTags}
+	}
+	return NormalizedTags, nil
 }
 
 // Validate the repo by attempting to query it's references.

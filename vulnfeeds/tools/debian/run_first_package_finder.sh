c9b751d0c2c6d8660f14ff4b2fda085b3fd3720d
Rex P <106129829+another-rex@users.noreply.github.com>
2023-03-22 17:36:12 +1300

    Update vulnfeeds/tools/debian/run_first_package_finder.sh
    Co-authored-by: Andrew Pollock <andrewpollock@users.noreply.github.com>

--- vulnfeeds/tools/debian/run_first_package_finder.sh
+++ vulnfeeds/tools/debian/run_first_package_finder.sh
@@ -25,7 +25,7 @@ echo "Finding first packages"
 pipenv run python3 first_package_finder.py
 
 echo "Syncing with cloud first_package_output"
-gsutil -q -m rsync -c -d 'first_package_output' gs://$OUTPUT_BUCKET/first_package_output
+gsutil -q -m rsync -c -d 'first_package_output' "gs://${OUTPUT_BUCKET}/first_package_output"
 echo "Successfully synced with cloud"
 
 popd

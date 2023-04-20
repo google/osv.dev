b2f5ae605e4c889e249fe59c06d0af390c3ed928
Rex Pan <rexpan@google.com>
2023-04-20 10:55:17 +1000

    Update caller

--- tools/indexer-api-caller/caller.go
+++ tools/indexer-api-caller/caller.go
@@ -1,25 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/base64"
 	"flag"
 	"fmt"
 	"io/fs"
 	"log"
-	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"sort"
 	"strings"
 )
 
 var (
-	repoDir  = flag.String("repo", "", "repo directory")
-	repo2Dir = flag.String("repo2", "", "repo directory")
-	outfile  = flag.String("out", "temp.txt", "output file")
-	// tagName = flag.String("tag", "", "tag name to checkout")
+	repoDir = flag.String("repo", "", "repo directory")
 )
 
 type Hash = []byte
@@ -30,43 +26,12 @@ type FileResult struct {
 	Hash Hash   `datastore:"hash"`
 }
 
-// FileResult holds the per file hash and path information.
-type TreeNode struct {
-	NodeHash       Hash   `datastore:"node_hash"`
-	ChildHashes    []Hash `datastore:"child_hashes,noindex"`
-	Height         int    `datastore:"depth,noindex"`
-	FilesContained int    `datastore:"files_contained,noindex"`
-}
-
 func main() {
 	flag.Parse()
-
-	fileRes := [][]*FileResult{}
-	// file2Res := [][]*FileResult{}
-	// buildGit(*repo2Dir, &file2Res)
-	buildGit(*repoDir, &fileRes)
-
-	// log.Println(strings.Join(pretty.Diff(fileRes, file2Res), "\n"))
+	buildGit(*repoDir)
 }
 
-func buildGit(repoDir string, out *[][]*FileResult) error {
-	// repo, err := git.PlainOpen(*repoDir)
-	// if err != nil {
-	// 	return fmt.Errorf("failed to open repo: %v", err)
-	// }
-	// tree, err := repo.Worktree()
-	// if err != nil {
-	// 	return fmt.Errorf("failed to get work tree: %v", err)
-	// }
-	// checkoutOptions = git.CheckoutOptions{
-	// 	Force: true,
-
-	// }
-	// repoInfo.CheckoutOptions.Force = true
-	// if err := tree.Checkout(); err != nil {
-	// 	return fmt.Errorf("failed to checkout tree: %v", err)
-	// }
-
+func buildGit(repoDir string) error {
 	fileExts := []string{
 		".hpp",
 		".h",
@@ -75,7 +40,7 @@ func buildGit(repoDir string, out *[][]*FileResult) error {
 		".c",
 		".cpp",
 	}
-	log.Printf("%v", repoDir)
+
 	var fileResults []*FileResult
 	if err := filepath.Walk(repoDir, func(p string, info fs.FileInfo, err error) error {
 		if info.IsDir() {
@@ -99,27 +64,12 @@ func buildGit(repoDir string, out *[][]*FileResult) error {
 		return fmt.Errorf("failed during file walk: %v", err)
 	}
 
-	sort.Slice(fileResults, func(i, j int) bool {
-		for k := 0; k < len(fileResults[i].Hash); k++ {
-			if fileResults[i].Hash[k] < fileResults[j].Hash[k] {
-				return true
-			}
-			if fileResults[i].Hash[k] > fileResults[j].Hash[k] {
-				return false
-			}
-		}
-		return false
-	})
-	_, fileRes := processTree(fileResults)
-	*out = fileRes
-
 	log.Printf("%v", len(fileResults))
 
 	b := strings.Builder{}
 	b.WriteString(`{"query": {"name":"protobuf", "file_hashes": [`)
 
 	for i, fr := range fileResults {
-		// strings.Join()
 		if i == len(fileResults)-1 {
 			fmt.Fprintf(&b, "{\"hash\": \"%s\"}", base64.StdEncoding.EncodeToString(fr.Hash))
 		} else {
@@ -127,18 +77,17 @@ func buildGit(repoDir string, out *[][]*FileResult) error {
 		}
 	}
 	b.WriteString("]}}")
-	// b.WriteRune('\n')
 
 	cmd := exec.Command("bash")
 	cmd.Args = append(cmd.Args, "-c", `grpcurl -plaintext -d @ -protoset api_descriptor.pb 127.0.0.1:8000 osv.v1.OSV/DetermineVersion`)
-	// log.Printf("%v", cmd)
-	pipe, err := cmd.StdinPipe()
+
+	buffer := bytes.Buffer{}
+	_, err := buffer.Write([]byte(b.String()))
 	if err != nil {
 		log.Panicln(err)
 	}
-	os.WriteFile(*outfile, []byte(b.String()), 0644)
-	pipe.Write([]byte(b.String()))
-	pipe.Close()
+
+	cmd.Stdin = &buffer
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
@@ -146,92 +95,5 @@ func buildGit(repoDir string, out *[][]*FileResult) error {
 	}
 
 	log.Println(string(output))
-	// ("bash", []string{"-c", b.String()}, nil)
-	// fmt.Print(b.String())
 	return nil
-	// log.Info("Begin processing tree")
-	// treeResults, bucketResults := processTree(fileResults)
-	// log.Info("Begin stroage")
-}
-
-const chunkSize = 4
-const bucketCount = 256
-
-func processTree(fileResults []*FileResult) ([][]*TreeNode, [][]*FileResult) {
-	// This height includes the root node (height of 1 is just the root)
-	heightOfTree := logWithBase(((chunkSize-1)*bucketCount)+1, chunkSize)
-	// Tree, 0 is the leaf layer
-	var results = make([][]*TreeNode, heightOfTree)
-	buckets := make([][]*FileResult, bucketCount)
-
-	for _, fr := range fileResults {
-		buckets[fr.Hash[0]] = append(buckets[fr.Hash[0]], fr)
-	}
-
-	// Create base layer
-	results[0] = make([]*TreeNode, bucketCount)
-
-	for bucketIdx := range buckets {
-		// Sort hashes
-		sort.Slice(buckets[bucketIdx], func(i, j int) bool {
-			for k := 0; k < len(buckets[bucketIdx][i].Hash); k++ {
-				if buckets[bucketIdx][i].Hash[k] < buckets[bucketIdx][j].Hash[k] {
-					return true
-				}
-				if buckets[bucketIdx][i].Hash[k] > buckets[bucketIdx][j].Hash[k] {
-					return false
-				}
-			}
-			return false
-		})
-
-		hasher := md5.New()
-		for _, v := range buckets[bucketIdx] {
-			_, err := hasher.Write(v.Hash)
-			if err != nil {
-				log.Panicf("Hasher error: %v", err)
-			}
-		}
-
-		results[0][bucketIdx] = &TreeNode{
-			NodeHash:       hasher.Sum(nil),
-			ChildHashes:    nil,
-			Height:         0,
-			FilesContained: len(buckets[bucketIdx]),
-		}
-	}
-
-	// Start building the higher layers
-	for height := 1; height < len(results); height++ {
-		results[height] = make([]*TreeNode, len(results[height-1])/chunkSize)
-		for i := 0; i < len(results[height-1]); i += chunkSize {
-			hasher := md5.New()
-			childHashes := []Hash{}
-			filesContained := 0
-			// log.Printf("height: %d, len: %d, %v\n", height, len(results[height-1]), results[height-1])
-
-			for _, v := range results[height-1][i : i+chunkSize] {
-				// log.Printf("%v\n", v.NodeHash)
-				_, err := hasher.Write(v.NodeHash)
-				childHashes = append(childHashes, v.NodeHash)
-				filesContained += v.FilesContained
-				if err != nil {
-					log.Panicf("Hasher error: %v", err)
-				}
-			}
-			parentIdx := i / chunkSize
-			results[height][parentIdx] = &TreeNode{
-				NodeHash:       hasher.Sum(nil),
-				ChildHashes:    childHashes,
-				Height:         height,
-				FilesContained: filesContained,
-			}
-		}
-	}
-
-	return results, buckets
-}
-
-func logWithBase(x int, base int) int {
-	return int(math.Ceil(math.Log(float64(x)) / math.Log(float64(base))))
 }
--- tools/indexer-api-caller/go.mod
+++ tools/indexer-api-caller/go.mod
@@ -1,19 +1,3 @@
 module github.com/google/indexer-api-caller
 
 go 1.19
-
-require (
-	github.com/kr/pretty v0.3.1
-	google.golang.org/grpc v1.54.0
-)
-
-require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/rogpeppe/go-internal v1.9.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
-)

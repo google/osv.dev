adc4f6fa6de0b14e8c0ad9e502434d68ac3ddedc
Rex P <106129829+another-rex@users.noreply.github.com>
2023-05-03 10:32:23 +1000

    Add last touches to the indexer API (#1250)
    and update Pipfile to include direct dependency of ndb.
    
    Changes to API:
    - Cutoff response after score is below 0.2
    - Adjust formula to use the adjusted bucket size that ignores the empty
    buckets.
    - Changed protobuf to make hash_type an enum
    - Made osv folder in api a symbolic link instead of copying it every
    time tests are ran. This follows what is done in v1 folder and in
    appengine.
    - Updated Pipfile dependencies to resolve conflicts. All latest versions
    of grpcio should work now.
    - Fully match against empty buckets via a empty bucket bitmap, this
    simplifies the estimation formula by a lot
    - Account for file count differences by storing file count in RepoIndex
    - Fixes bug where fetch is not pulling in new tags

---
+++ tools/datastore-remover/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"cloud.google.com/go/datastore"
+)
+
+var (
+	kind      = flag.String("kind", "", "kind to delete")
+	projectID = flag.String("project_id", "", "the gcp project ID")
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	flag.Parse()
+	if *kind == "" || *projectID == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Deleting kind: %s, in project: %s\nEnter yes to confirm: \n", *kind, *projectID)
+	scanner.Scan()
+	if scanner.Text() == "yes" {
+		client, _ := datastore.NewClient(ctx, *projectID)
+
+		keys, _ := client.GetAll(ctx, datastore.NewQuery(*kind).KeysOnly(), nil)
+		log.Printf("Retrieved %d keys", len(keys))
+		for i := 0; i < len(keys); i += 500 {
+			fmt.Printf("Deleting %d number now\n", i)
+			err := client.DeleteMulti(ctx, keys[i:min(i+500, len(keys))])
+			if err != nil {
+				log.Fatalf("%v", err)
+			}
+		}
+		fmt.Printf("%v\n", len(keys))
+	} else {
+		fmt.Println("Not yes entered, exiting")
+		os.Exit(1)
+	}
+}

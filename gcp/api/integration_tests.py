fc7e59d29509405c92fd972087e6094f93f02184
Rex P <rexpan@google.com>
2023-05-15 15:24:56 +1000

    Fix lints
    Signed-off-by: Rex P <rexpan@google.com>

--- gcp/api/integration_tests.py
+++ gcp/api/integration_tests.py
@@ -274,7 +274,8 @@ class IntegrationTests(unittest.TestCase):
         timeout=_TIMEOUT)
     self.assert_results_equal({'vulns': expected_vulns}, response.json())
 
-  def test_no_vulns(self):
+  def test_query_semver_no_vulns(self):
+    """Test queries by SemVer with no vulnerabilities."""
     package = 'github.com/justinas/nosurf'
     ecosystem = 'Go'
 

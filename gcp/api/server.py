06e5ed57f731c0ada2bc6f56502fc0cf2e7b56a9
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-08-08 14:39:10 +0530

    removed trailing spaces

--- gcp/api/server.py
+++ gcp/api/server.py
@@ -214,9 +214,9 @@ class OSVServicer(osv_service_v1_pb2_grpc.OSVServicer,
 # Wrapped in a separate class
 @dataclass
 class ResponsesCount:
-  """Wraps responses count in a separate class 
+  """Wraps responses count in a separate class
   to allow it to be passed by reference
-  
+
   Also adds a interface to allow easy updating to a mutex
   if necessary
   """
@@ -234,7 +234,7 @@ class ResponsesCount:
 
   def page_limit(self) -> int:
     """
-    Returns the limit based on whether we have 
+    Returns the limit based on whether we have
     exceeded the _MAX_VULN_RESP_THRESH with the total number
     of responses in the entire query batch
     """
@@ -256,7 +256,7 @@ class QueryContext:
 def process_buckets(
     file_results: List[osv.FileResult]) -> List[osv.RepoIndexBucket]:
   """
-  Create buckets in the same process as 
+  Create buckets in the same process as
   indexer to generate the same bucket hashes
   """
   buckets: list[list[bytes]] = [[] for _ in range(_BUCKET_SIZE)]
@@ -368,7 +368,7 @@ def build_determine_version_result(
 
 def estimate_diff(num_bucket_change: int, file_count_diff: int) -> int:
   """
-  Estimates the number of files that have changed based on 
+  Estimates the number of files that have changed based on
   the number of buckets that changed.
   """
   estimate = _BUCKET_SIZE * math.log(
@@ -437,11 +437,8 @@ def determine_version(version_query: osv_service_v1_pb2.VersionQuery,
                                         len(version_query.file_hashes))
 
 def validate_package_name(package_name):
-    if len(package_name) > MAX_PACKAGE_NAME_LENGTH:
-        raise ValueError("Package name exceeds the maximum allowed length.")
-    
-
-
+  if len(package_name) > MAX_PACKAGE_NAME_LENGTH:
+    raise ValueError("Package name exceeds the maximum allowed length.")
 @ndb.tasklet
 def do_query(query, context: QueryContext, include_details=True):
   """Do a query."""
@@ -791,8 +788,6 @@ def query_by_version(context: QueryContext,
                      version,
                      to_response: Callable = bug_to_response):
   """Query by (fuzzy) version."""
-  
-
   if package_name:
     query = osv.Bug.query(
         osv.Bug.status == osv.BugStatus.PROCESSED,

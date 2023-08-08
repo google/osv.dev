e4f5d71b17b7350faa093e834366c623b4b913b8
Tyler <tylfin@gmail.com>
2023-08-08 14:04:49 -0400

    Fix: Linter issues and fetching Bioconductor versions

--- osv/ecosystems/bioconductor.py
+++ osv/ecosystems/bioconductor.py
@@ -19,49 +19,67 @@ from . import config
 from .helper_base import Ecosystem, EnumerateError
 from .. import semver_index
 
+
 class Bioconductor(Ecosystem):
   """Bioconductor ecosystem helpers."""
 
-  # Use the Posit Public Package Manager API to pull both the current and older versions for a specific package
-  # since Bioconductor doesn't natively support this functionality.
-  _API_PACKAGE_URL_POSIT_BIOCONDUCTOR = 'https://packagemanager.posit.co/__api__/repos/4/packages/{package}?bioc_version={bioc_version}'
+  # Use the Posit Public Package Manager API to pull both the current and
+  # older versions for a specific package since Bioconductor doesn't natively
+  # support this functionality.
+  _API_PACKAGE_URL_POSIT_BIOCONDUCTOR = \
+    'https://packagemanager.posit.co/__api__/repos/4/packages/' + \
+    '{package}?bioc_version={bioc_version}'
   _API_BIOC_VERSIONS_URL = 'https://packagemanager.posit.co/__api__/status'
-  _BIOC_VERSIONS = ['3.18', '3.17', '3.16', '3.15', '3.14', '3.13', '3.12', '3.11', '3.10', '3.9', '3.8', '3.7', '3.6', '3.5', '3.4', '3.3', '3.2', '3.1']
 
-  def refresh_bioc_versions(self):
+  def get_bioc_versions(self):
     """
-    Pull latest Bioconductor versions
+    get latest Bioconductor versions
     """
     response = requests.get(self._API_BIOC_VERSIONS_URL)
     data = response.json()
+    if response.status_code == 404:
+      raise RuntimeError('Failed to get Bioconducot versions')
 
-    self._BIOC_VERSIONS = [bioc['bioc_version'] for bioc in data['bioc_versions']]
+    return [bioc['bioc_version'] for bioc in data['bioc_versions']]
 
   def sort_key(self, version):
     """Sort key."""
     return semver_index.parse(version)
 
-  def _enumerate_versions(self, url, package, introduced, fixed=None, last_affected=None, limits=None):
+  def _enumerate_versions(self,
+                          url,
+                          bioc_versions,
+                          package,
+                          introduced,
+                          fixed=None,
+                          last_affected=None,
+                          limits=None):
     """Helper method to enumerate versions from a specific URL."""
 
     versions = []
-    for version in self._BIOC_VERSIONS:
-      response = requests.get(url.format(package=package, bioc_version=version), timeout=config.timeout)
+    for version in bioc_versions:
+      response = requests.get(
+          url.format(package=package, bioc_version=version),
+          timeout=config.timeout)
       if response.status_code == 404:
-        # Break here as we're starting at latest and going back as packages won't disappear.
+        # Break here as we're starting at latest and going back
+        # as packages won't disappear.
         break
-      elif response.status_code != 200:
-        raise RuntimeError(f'Failed to get R versions for {package} with: {response.text}')
+
+      if response.status_code != 200:
+        raise RuntimeError(
+            f'Failed to get R versions for {package} with: {response.text}')
 
       response = response.json()
       if 'version' in response:
-          versions += [response['version']]
+        versions += [response['version']]
 
     if len(versions) == 0:
       raise EnumerateError(f'Package {package} not found')
 
     self.sort_versions(versions)
-    return self._get_affected_versions(versions, introduced, fixed, last_affected, limits)
+    return self._get_affected_versions(versions, introduced, fixed,
+                                       last_affected, limits)
 
   def enumerate_versions(self,
                          package,
@@ -71,14 +89,10 @@ class Bioconductor(Ecosystem):
                          limits=None):
     """Enumerate versions."""
     # Refresh to ensure any new Bioconductor versions are included
-    self.refresh_bioc_versions()
+    bioc_versions = self.get_bioc_versions()
     versions = self._enumerate_versions(
-        self._API_PACKAGE_URL_POSIT_BIOCONDUCTOR,
-        package,
-        introduced,
-        fixed,
-        last_affected,
-        limits)
+        self._API_PACKAGE_URL_POSIT_BIOCONDUCTOR, bioc_versions, package,
+        introduced, fixed, last_affected, limits)
 
     if versions is None:
       raise EnumerateError(f'Package {package} not found')
--- osv/ecosystems/cran.py
+++ osv/ecosystems/cran.py
@@ -19,39 +19,52 @@ import packaging.version
 from . import config
 from .helper_base import Ecosystem, EnumerateError
 
+
 class CRAN(Ecosystem):
   """CRAN ecosystem helpers."""
 
-  # Use the Posit Public Package Manager API to pull both the current and archived versions for a specific package
-  # since CRAN doesn't natively support this functionality.
-  _API_PACKAGE_URL_POSIT_CRAN = 'https://packagemanager.posit.co/__api__/repos/2/packages/{package}'
+  # Use the Posit Public Package Manager API to pull both the current
+  # and archived versions for a specific package since CRAN doesn't
+  # natively support this functionality.
+  _API_PACKAGE_URL_POSIT_CRAN = 'https://packagemanager.posit.co/__api__/' + \
+    'repos/2/packages/{package}'
 
   def sort_key(self, version):
     """Sort key."""
     # Some documentation on CRAN versioning and the R numeric_version method:
-    # - https://cran.r-project.org/doc/manuals/R-exts.html#The-DESCRIPTION-file
-    # - https://stat.ethz.ch/R-manual/R-devel/library/base/html/numeric_version.html
-    # The packaging.version appears to work for the typical X.Y.Z and X.Y-Z cases
+    # https://cran.r-project.org/doc/manuals/R-exts.html#The-DESCRIPTION-file
+    # https://stat.ethz.ch/R-manual/R-devel/library/base/html/numeric_version.html
+    # The packaging.version appears to work for the typical X.Y.Z and
+    # X.Y-Z cases
     version = version.replace("-", ".")
     return packaging.version.parse(version)
 
-  def _enumerate_versions(self, url, package, introduced, fixed=None, last_affected=None, limits=None):
+  def _enumerate_versions(self,
+                          url,
+                          package,
+                          introduced,
+                          fixed=None,
+                          last_affected=None,
+                          limits=None):
     """Helper method to enumerate versions from a specific URL."""
     response = requests.get(url.format(package=package), timeout=config.timeout)
     if response.status_code == 404:
       return None
-    elif response.status_code != 200:
-      raise RuntimeError(f'Failed to get R versions for {package} with: {response.text}')
+
+    if response.status_code != 200:
+      raise RuntimeError(
+          f'Failed to get R versions for {package} with: {response.text}')
 
     response = response.json()
     versions = []
     if 'version' in response:
-        versions = [response['version']]
+      versions = [response['version']]
     if 'archived' in response:
-        versions += [archived['version'] for archived in response['archived']]
+      versions += [archived['version'] for archived in response['archived']]
 
     self.sort_versions(versions)
-    return self._get_affected_versions(versions, introduced, fixed, last_affected, limits)
+    return self._get_affected_versions(versions, introduced, fixed,
+                                       last_affected, limits)
 
   def enumerate_versions(self,
                          package,
@@ -60,13 +73,9 @@ class CRAN(Ecosystem):
                          last_affected=None,
                          limits=None):
     """Enumerate versions."""
-    versions = self._enumerate_versions(
-        self._API_PACKAGE_URL_POSIT_CRAN,
-        package,
-        introduced,
-        fixed,
-        last_affected,
-        limits)
+    versions = self._enumerate_versions(self._API_PACKAGE_URL_POSIT_CRAN,
+                                        package, introduced, fixed,
+                                        last_affected, limits)
 
     if versions is None:
       raise EnumerateError(f'Package {package} not found')

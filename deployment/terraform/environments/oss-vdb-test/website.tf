366423402030335c7030620b3baa42cab1a4c2e6
Michael Kedar <michaelkedar@google.com>
2023-08-15 14:00:33 +1000

    Terraform iam unauthenticated access

--- deployment/terraform/environments/oss-vdb-test/website.tf
+++ deployment/terraform/environments/oss-vdb-test/website.tf
@@ -1,7 +1,7 @@
 # TODO(michaelkedar):
 # Temporary Terraform config to mirror the app engine onto Cloud Run for the test instance only
 # Should be deleted and remade in the modules folder when formally moving away from App Engine
-resource "google_cloud_run_service" "website" {
+resource "google_cloud_run_v2_service" "website" {
   project  = "oss-vdb-test"
   name     = "osv-website"
   location = "us-west2"
@@ -27,6 +27,16 @@ resource "google_cloud_run_service" "website" {
     ]
     # prevent_destroy = true
   }
+}
+
+# Allow unauthenticated access
+resource "google_cloud_run_service_iam_binding" "website" {
+  location = google_cloud_run_v2_service.website.location
+  service  = google_cloud_run_v2_service.website.name
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
 }
 
 # TODO(michaelkedar): Native Cloud Run domain mapping does not work on us-west2

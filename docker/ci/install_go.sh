google/osv.dev

commit 064a3cb686cf44a66b061e914e52c9a6d9f4ecd1

Standardise on the same version of the Go buildchain as the CI image (#1549)
Until we arrive at a "one buildchain image to rule them all" solution,
to avoid Go buildchain skew, lock everything, everywhere (manually ☹) to
1.21

docker/ci/install_go.sh
@@ -20,5 +20,5 @@ set -eux
 # Download and install Go
 wget https://storage.googleapis.com/golang/getgo/installer_linux -O $1/installer_linux
 chmod +x $1/installer_linux
-SHELL="bash" $1/installer_linux -version 1.20
+SHELL="bash" $1/installer_linux -version 1.21
 rm $1/installer_linux

docker/indexer/Dockerfile
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM golang:1.20 as GO_BUILD
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 WORKDIR /build
 
 # Cache dependencies in these steps

docker/terraform/Dockerfile
@@ -1,14 +1,14 @@
 # Taken and modified from https://github.com/GoogleCloudPlatform/cloud-builders-community/tree/master/terraform
 
-FROM golang:1.20 AS build
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 
 ARG TERRAFORM_VERSION
 WORKDIR /build/
 RUN GOBIN=$(pwd) go install github.com/hashicorp/terraform@v${TERRAFORM_VERSION}
 
-FROM gcr.io/cloud-builders/gcloud
+FROM gcr.io/google.com/cloudsdktool/cloud-sdk:alpine
 
-COPY --from=build /build/terraform /usr/bin/terraform
+COPY --from=GO_BUILD /build/terraform /usr/bin/terraform
 COPY entrypoint.bash /builder/entrypoint.bash
 
 ENTRYPOINT ["/builder/entrypoint.bash"]

vulnfeeds/cmd/alpine/Dockerfile
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM golang:alpine AS GO_BUILD
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 
 RUN mkdir /src
 WORKDIR /src

vulnfeeds/cmd/combine-to-osv/Dockerfile
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM golang:alpine AS GO_BUILD
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 
 RUN mkdir /src
 WORKDIR /src

vulnfeeds/cmd/cperepos/Dockerfile
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM golang:alpine@sha256:7839c9f01b5502d7cb5198b2c032857023424470b3e31ae46a8261ffca72912a
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 
 RUN mkdir /src
 WORKDIR /src
@@ -26,7 +26,7 @@ RUN CGO_ENABLED=0 go build -o cperepos ./cmd/cperepos
 
 FROM gcr.io/google.com/cloudsdktool/google-cloud-cli:alpine
 
-COPY --from=0 /src/cperepos ./
+COPY --from=GO_BUILD /src/cperepos ./
 COPY ./cmd/cperepos/gen_cperepos_map ./
 
 ENTRYPOINT ["/gen_cperepos_map"]

vulnfeeds/cpp/Dockerfile
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-FROM golang:alpine@sha256:6f592e0689192b7e477313264bb190024d654ef0a08fb1732af4f4b498a2e8ad AS buildenv
+FROM golang:1.21-alpine@sha256:445f34008a77b0b98bf1821bf7ef5e37bb63cc42d22ee7c21cc17041070d134f AS GO_BUILD
 
 WORKDIR /go/src
 
@@ -24,7 +24,7 @@ RUN CGO_ENABLED=0 go build -v -o /usr/local/bin ./cpp ./cmd/download-cves
 
 FROM gcr.io/google.com/cloudsdktool/google-cloud-cli:alpine AS runtime
 
-COPY --from=buildenv /usr/local/bin ./usr/local/bin
-COPY --from=buildenv /go/src/cpp/run_cve_to_osv_generation ./usr/local/bin
+COPY --from=GO_BUILD /usr/local/bin ./usr/local/bin
+COPY --from=GO_BUILD /go/src/cpp/run_cve_to_osv_generation ./usr/local/bin
 
 CMD ["/usr/local/bin/run_cve_to_osv_generation"]

commit 26f60d84602ef92a2bdbfdbae246b775ec645499

Ci use go1.21.1 (#1695)
Install Go 1.21.1 in the CI image as it is actually installable via this
installation method.

Works around https://github.com/golang/go/issues/63367 and fixes what
was introduced in #1549

Adjust the installation method to match current documentation and add
some signposting to it.

docker/ci/Dockerfile
@@ -23,7 +23,7 @@ RUN apt-get update && \
 COPY daemon.json /etc/docker/daemon.json
 COPY install_go.sh /tmp/install_go.sh
 
-RUN "/tmp/install_go.sh" "/tmp"
+RUN "/tmp/install_go.sh"
 ENV PATH "$PATH:/root/.go/bin:/root/go/bin"
 
 RUN go install -tags extended github.com/gohugoio/hugo@v0.111.3

docker/ci/install_go.sh
@@ -18,7 +18,5 @@
 set -eux
 
 # Download and install Go
-wget https://storage.googleapis.com/golang/getgo/installer_linux -O $1/installer_linux
-chmod +x $1/installer_linux
-SHELL="bash" $1/installer_linux -version 1.21
-rm $1/installer_linux
+# https://pkg.go.dev/golang.org/x/tools/cmd/getgo#section-readme
+curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && SHELL="bash" ./go_installer -version 1.21.1 && rm go_installer

vulnfeeds/go.mod
@@ -1,6 +1,6 @@
 module github.com/google/osv/vulnfeeds
 
-go 1.19
+go 1.21
 
 require (
 	cloud.google.com/go/logging v1.8.1
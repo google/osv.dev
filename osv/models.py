a452b2ca681345a946f951f9e3381106ed816e3a
Jess Lowe <jesslowe@google.com>
2024-02-09 04:27:48 +0000

    fix lint

--- osv/models.py
+++ osv/models.py
@@ -374,7 +374,8 @@ class Bug(ndb.Model):
         for pkg in self.affected_packages
         if pkg.package.ecosystem
     }
-    # Only attempt to add the Git ecosystem if there are no existing ecosystems present
+    # Only attempt to add the Git ecosystem if 
+    # there are no existing ecosystems present
     if not ecosystems_set:
       for pkg in self.affected_packages:
         for r in pkg.ranges:

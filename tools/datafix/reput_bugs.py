5fbb52580fa15e216078e9fa9bcfec6950028879
Jess Lowe <jesslowe@google.com>
2024-02-09 01:53:54 +0000

    Added timekeeping for reputting and changed GIT ecosystem to only be added if no other ecosystem

--- tools/datafix/reput_bugs.py
+++ tools/datafix/reput_bugs.py
@@ -6,6 +6,7 @@
 """
 from google.cloud import ndb
 import osv
+import time
 
 import argparse
 
@@ -14,28 +15,33 @@ MAX_BATCH_SIZE = 500
 
 def reput_bugs(dryrun: bool, source: str) -> None:
   """ Reput all bugs from a given source."""
-  query = osv.Bug.query().filter(osv.Bug.source == source)
+  query = osv.Bug.query()
+  query = query.filter(osv.Bug.source == source)
   print(f"Running query {query.filters} "
         f"on {query.kind}...")
 
   result = list(query.fetch(keys_only=True))
+  result.sort(key=lambda r: r.id())
+  # result = [r for r in result if not r.id()[0].isnumeric()]
   print(f"Retrieved {len(result)} bugs to examine for reputting")
-
+  num_reputted = 0
+  time_start = time.perf_counter()
   # This handles the actual transaction of reputting the bugs with ndb
   def _reput_ndb():
     # Reputting the bug runs the Bug _pre_put_hook() in models.py
-    print(f"Reputting {len(result[batch:batch + MAX_BATCH_SIZE])} bugs...")
     if dryrun:
       print("Dry run mode. Preventing transaction from commiting")
       raise Exception("Dry run mode")  # pylint: disable=broad-exception-raised
-
     ndb.put_multi_async([
         osv.Bug.get_by_id(r.id()) for r in result[batch:batch + MAX_BATCH_SIZE]
     ])
+    print(f"Time elapsed: {(time.perf_counter() - time_start):.2f} seconds.")
 
   # Chunk the results to reput in acceptibly sized batches for the API.
   for batch in range(0, len(result), MAX_BATCH_SIZE):
     try:
+      num_reputted += len(result[batch:batch + MAX_BATCH_SIZE])
+      print(f"Reput {num_reputted} bugs... - {num_reputted/len(result)*100:.2f}%")
       ndb.transaction(_reput_ndb)
     except Exception as e:
       # Don't have the first batch's transaction-aborting exception stop
@@ -43,6 +49,7 @@ def reput_bugs(dryrun: bool, source: str) -> None:
       if dryrun and e.args[0].startswith("Dry run mode"):
         print("Dry run mode. Preventing transaction from commiting")
       else:
+        print([r.id() for r in result[batch:batch + MAX_BATCH_SIZE]])
         print(f"Exception {e} occurred. Continuing to next batch.")
 
   print("Reputted!")

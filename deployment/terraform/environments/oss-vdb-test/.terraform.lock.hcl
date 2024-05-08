b0f0d5d7ae6ebfb51a91e504a44d2b24f16e3208
Michael Kedar <michaelkedar@google.com>
2024-05-08 14:53:44 +1000

    Configure GCLB in terraform

--- deployment/terraform/environments/oss-vdb-test/.terraform.lock.hcl
+++ deployment/terraform/environments/oss-vdb-test/.terraform.lock.hcl
@@ -80,3 +80,23 @@ provider "registry.terraform.io/hashicorp/null" {
     "zh:c23084e1b23577de22603cff752e59128d83cfecc2e6819edadd8cf7a10af11e",
   ]
 }
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.6.1"
+  constraints = ">= 2.1.0"
+  hashes = [
+    "h1:1OlP753r4lOKlBprL0HdZGWerm5DCabD5Mli8k8lWAg=",
+    "zh:2a0ec154e39911f19c8214acd6241e469157489fc56b6c739f45fbed5896a176",
+    "zh:57f4e553224a5e849c99131f5e5294be3a7adcabe2d867d8a4fef8d0976e0e52",
+    "zh:58f09948c608e601bd9d0a9e47dcb78e2b2c13b4bda4d8f097d09152ea9e91c5",
+    "zh:5c2a297146ed6fb3fe934c800e78380f700f49ff24dbb5fb5463134948e3a65f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7ce41e26f0603e31cdac849085fc99e5cd5b3b73414c6c6d955c0ceb249b593f",
+    "zh:8c9e8d30c4ef08ee8bcc4294dbf3c2115cd7d9049c6ba21422bd3471d92faf8a",
+    "zh:93e91be717a7ffbd6410120eb925ebb8658cc8f563de35a8b53804d33c51c8b0",
+    "zh:982542e921970d727ce10ed64795bf36c4dec77a5db0741d4665230d12250a0d",
+    "zh:b9d1873f14d6033e216510ef541c891f44d249464f13cc07d3f782d09c7d18de",
+    "zh:cfe27faa0bc9556391c8803ade135a5856c34a3fe85b9ae3bdd515013c0c87c1",
+    "zh:e4aabf3184bbb556b89e4b195eab1514c86a2914dd01c23ad9813ec17e863a8a",
+  ]
+}
--- deployment/terraform/modules/osv/website.tf
+++ deployment/terraform/modules/osv/website.tf
@@ -1,3 +1,4 @@
+# Website Cloud Run service
 resource "google_cloud_run_v2_service" "website" {
   project  = var.project_id
   name     = "osv-website"
@@ -32,4 +33,122 @@ resource "google_cloud_run_service_iam_binding" "website" {
   ]
 }
 
-# TODO: Set up Google Cloud Load Balancing + Network Endpoint Group (NEG)
+# SSL Certificates
+resource "google_certificate_manager_dns_authorization" "website" {
+  project     = var.project_id
+  name        = "website-dnsauth"
+  description = "The dns auth for the osv.dev website"
+  domain      = var.website_domain
+}
+
+resource "google_certificate_manager_certificate" "website" {
+  project     = var.project_id
+  name        = "website-cert"
+  description = "The osv.dev website cert"
+  managed {
+    domains = [var.website_domain, "*.${var.website_domain}"]
+    dns_authorizations = [
+      google_certificate_manager_dns_authorization.website.id
+    ]
+  }
+  lifecycle {
+    replace_triggered_by = [google_certificate_manager_dns_authorization.website.id]
+  }
+}
+
+resource "google_certificate_manager_certificate_map" "website" {
+  project     = var.project_id
+  name        = "website-certmap"
+  description = "osv.dev website certificate map"
+}
+
+resource "google_certificate_manager_certificate_map_entry" "website" {
+  project      = var.project_id
+  name         = "website-certmap-entry"
+  description  = "osv.dev website certificate map entry"
+  map          = google_certificate_manager_certificate_map.website.name
+  certificates = [google_certificate_manager_certificate.website.id]
+  hostname     = var.website_domain
+
+  lifecycle {
+    replace_triggered_by = [google_certificate_manager_certificate_map.website.id, google_certificate_manager_certificate.website.id]
+  }
+}
+
+# Load Balancer
+module "gclb" {
+  source  = "terraform-google-modules/lb-http/google//modules/serverless_negs"
+  version = "~> 10.0"
+
+  name    = "website"
+  project = var.project_id
+
+  enable_ipv6         = true
+  create_ipv6_address = true
+  ssl                 = true
+  certificate_map     = google_certificate_manager_certificate_map.website.id
+
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+
+  backends = {
+    default = {
+      groups = [
+        {
+          # TODO(michaelkedar): Replace with Cloud Run when ready
+          group = google_compute_region_network_endpoint_group.appengine_neg.id
+        }
+      ]
+      protocol   = "HTTPS"
+      enable_cdn = true
+      cdn_policy = {
+        cache_key_policy = {
+          include_host         = true
+          include_protocol     = true
+          include_query_string = true
+        }
+        signed_url_cache_max_age_sec = 0
+      }
+
+      iap_config = {
+        enable = false
+      }
+      log_config = {
+        enable = false
+      }
+    }
+  }
+}
+
+resource "google_compute_region_network_endpoint_group" "serverless_neg" {
+  project               = var.project_id
+  name                  = "serverless-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = google_cloud_run_v2_service.website.location
+  cloud_run {
+    service = google_cloud_run_v2_service.website.name
+  }
+}
+
+resource "google_compute_region_network_endpoint_group" "appengine_neg" {
+  project               = var.project_id
+  name                  = "appengine-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = google_app_engine_application.app.location_id
+  app_engine {}
+}
+
+# Output all the DNS records required for the website in one place. 
+output "website_dns_records" {
+  description = "DNS records that need to be created for the osv.dev website"
+  value = concat([
+    {
+      data = module.gclb.external_ip
+      name = "${var.website_domain}."
+      type = "A"
+    },
+    {
+      data = module.gclb.external_ipv6_address
+      name = "${var.website_domain}."
+      type = "AAAA"
+  }], google_certificate_manager_dns_authorization.website.dns_resource_record)
+}

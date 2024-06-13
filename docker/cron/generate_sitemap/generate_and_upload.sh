612127028d8843488efbf868f3d4fd561796ab1f
Rex P <106129829+another-rex@users.noreply.github.com>
2024-06-13 16:40:09 +1000

    Fix sitemap generate and upload script (#2310)
    The CWD is not in the same directory as the script. Use the script from
    the global PATH.

--- docker/cron/generate_sitemap/generate_and_upload.sh
+++ docker/cron/generate_sitemap/generate_and_upload.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
 SITEMAP_OUTPUT="sitemap_output/"
 OUTPUT_BUCKET="${OUTPUT_GCS_BUCKET:=test-osv-dev-sitemap}"
 BASE_URL_PATH="${BASE_URL:=https://test.osv.dev}"
 
 echo "Begin sitemap generation for $BASE_URL_PATH"
 
-./generate_sitemap.py --base_url $BASE_URL_PATH
+"$SCRIPT_PATH/generate_sitemap.py" --base_url $BASE_URL_PATH
 
 echo "Begin Syncing with cloud to $OUTPUT_BUCKET"
 

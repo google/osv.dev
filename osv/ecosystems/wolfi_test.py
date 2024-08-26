57050dbbf5172054a7e10e977cba8a69bb676050
Holly Gong <gongh@google.com>
2024-08-26 14:30:21 +1000

    feat(api): support Chainguard and Wolfi version query on test instance.

---
+++ osv/ecosystems/wolfi_test.py
@@ -0,0 +1,36 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Wolfi ecosystem helper tests."""
+
+import unittest
+from .. import ecosystems
+
+
+class WolfiEcosystemTest(unittest.TestCase):
+  """Wolfi ecosystem helper tests."""
+
+  def test_wolfi(self):
+    """Test sort_key"""
+    ecosystem = ecosystems.get('Wolfi')
+    self.assertGreater(
+        ecosystem.sort_key('38.52.0-r0'),
+        ecosystem.sort_key('37.52.0-r0'))
+    self.assertLess(
+        ecosystem.sort_key('453'),
+        ecosystem.sort_key('453-r1'))
+    self.assertGreater(ecosystem.sort_key('5.4.13-r1'), ecosystem.sort_key('0'))
+    self.assertGreater(
+        ecosystem.sort_key('1.4.0-r1'), ecosystem.sort_key('1.4.0-r0'))
+    self.assertGreater(
+        ecosystem.sort_key('invalid'), ecosystem.sort_key('1.4.0-r0'))

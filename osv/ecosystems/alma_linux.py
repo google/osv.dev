39512e7939571c6d84669b514f420a1701dbddb9
Holly Gong <gongh@google.com>
2024-08-27 14:49:53 +1000

    feat: support non-enumerated ecosystem version queries on prod instance

--- osv/ecosystems/alma_linux.py
+++ osv/ecosystems/alma_linux.py
@@ -31,3 +31,7 @@ class AlmaLinux(Ecosystem):
                          last_affected=None,
                          limits=None):
     raise NotImplementedError('Ecosystem helper does not support enumeration')
+
+  @property
+  def supports_comparing(self):
+    return True

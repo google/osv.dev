f839558cef968f163cd0e54c8d9cb966c9cd18ac
Oliver Chang <oliverchang@users.noreply.github.com>
2024-09-30 16:10:58 +1000

    chore: Migrate make_bugs_public to use Google issue tracker. (#2676)
    OSS-Fuzz migrated to Google Issue Tracker from Monorail as part of
    Monorail being turned down.
    
    Client based on a very stripped down implementation of
    https://github.com/google/clusterfuzz/tree/e2c3d679fa6e4eac32718afa362c83cfb14fe06c/src/clusterfuzz/_internal/issue_management/google_issue_tracker,
    
    with some changes to make auth work (via impersonation).

---
+++ docker/cron/make_bugs_public/google_issue_tracker/client.py
@@ -0,0 +1,64 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Gets a Google Issue Tracker HTTP client."""
+
+import google.auth
+from google.auth import impersonated_credentials
+import google_auth_httplib2
+from googleapiclient import discovery
+from googleapiclient import errors
+import httplib2
+
+_DISCOVERY_URL = ('https://issuetracker.googleapis.com/$discovery/rest?'
+                  'version=v1&labels=GOOGLE_PUBLIC')
+_SCOPE = 'https://www.googleapis.com/auth/buganizer'
+# OSS-Fuzz service account.
+_IMPERSONATED_SERVICE_ACCOUNT = (
+    '877343783628-compute@developer.gserviceaccount.com')
+_REQUEST_TIMEOUT = 60
+
+HttpError = errors.HttpError
+UnknownApiNameOrVersion = errors.UnknownApiNameOrVersion
+
+
+def build_http():
+  """Builds a httplib2.Http."""
+  source_credentials, _ = google.auth.default()
+  credentials = impersonated_credentials.Credentials(
+      source_credentials=source_credentials,
+      target_principal=_IMPERSONATED_SERVICE_ACCOUNT,
+      target_scopes=[_SCOPE])
+
+  return google_auth_httplib2.AuthorizedHttp(
+      credentials, http=httplib2.Http(timeout=_REQUEST_TIMEOUT))
+
+
+def _call_discovery(api, http):
+  """Calls the discovery service.
+
+  Retries upto twice if there are any UnknownApiNameOrVersion errors.
+  """
+  return discovery.build(
+      api,
+      'v1',
+      discoveryServiceUrl=_DISCOVERY_URL,
+      http=http,
+      static_discovery=False)
+
+
+def build(api='issuetracker', http=None):
+  """Builds a google api client for buganizer."""
+  if not http:
+    http = build_http()
+  return _call_discovery(api, http)

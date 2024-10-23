5c3c1802bb81744e9a1f8b7022b2c3c32b4bd0d2
Holly Gong <gongh@google.com>
2024-10-23 15:02:54 +1100

    fix lint

--- docker/staging_api_test/retrieve_bugs_from_bucket.py
+++ docker/staging_api_test/retrieve_bugs_from_bucket.py
@@ -30,6 +30,7 @@ BUG_DIR = './all_bugs'
 VULN_BUCKET = 'osv-test-vulnerabilities'
 ZIP_FILE_PATH = 'all.zip'
 
+
 def format_bug_for_output(bug: dict[str, any]) -> dict[str, any]:
   """Extracts relevant information from a vulnerability record.
 
@@ -43,17 +44,13 @@ def format_bug_for_output(bug: dict[str, any]) -> dict[str, any]:
     A dict storing all the important `Bug` fields that we want to use later
   """
   if not bug.get('affected'):
-    return {
-      'db_id': bug['id']
-    }
+    return {'db_id': bug['id']}
 
   affected_fuzzy = None
   affected = random.choice(bug['affected'])
   affected_package = affected.get('package')
   if not affected_package:
-    return {
-      'db_id': bug['id']
-    }
+    return {'db_id': bug['id']}
 
   # Store one version for use as the query version later.
   if affected.get('versions'):
@@ -73,6 +70,7 @@ def format_bug_for_output(bug: dict[str, any]) -> dict[str, any]:
       'affected_fuzzy': affected_fuzzy
   }
 
+
 def download_vuln_zip(tmp_dir: str) -> None:
   """Downloads all.zip file from bucket."""
   logging.info('Start to download %s.', ZIP_FILE_PATH)
@@ -80,11 +78,12 @@ def download_vuln_zip(tmp_dir: str) -> None:
   bucket = storage_client.get_bucket(VULN_BUCKET)
   try:
     blob = bucket.blob(ZIP_FILE_PATH)
-    file_path = os.path.join(tmp_dir, ZIP_FILE_PATH)  
+    file_path = os.path.join(tmp_dir, ZIP_FILE_PATH)
     blob.download_to_filename(file_path)
   except Exception as e:
     logging.exception('Failed to download all.zip: %s', e)
   logging.info('Downloaded %s.', ZIP_FILE_PATH)
+
 
 def get_bugs_from_export() -> None:
   """Gets all bugs from the exported all.zip and writes to `BUG_DIR`."""
@@ -108,10 +107,10 @@ def get_bugs_from_export() -> None:
     download_vuln_zip(tmp_dir)
     all_zip = os.path.join(tmp_dir, ZIP_FILE_PATH)
     bug_info_list = []
-    with zipfile.ZipFile(all_zip, 'r') as zip:
-      for filename in zip.namelist():
+    with zipfile.ZipFile(all_zip, 'r') as vuln_zip:
+      for filename in vuln_zip.namelist():
         try:
-          with zip.open(filename) as file:
+          with vuln_zip.open(filename) as file:
             bug = json.load(file)
             bug_info_list.append(format_bug_for_output(bug))
             if len(bug_info_list) == entries_per_file:
@@ -123,6 +122,7 @@ def get_bugs_from_export() -> None:
           continue
 
   logging.info('All results saved to %s.', BUG_DIR)
+
 
 def main() -> None:
   osv.logs.setup_gcp_logging('staging-test')

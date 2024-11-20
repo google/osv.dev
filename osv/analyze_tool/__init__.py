32d325cda2a82a5760852440a5bc7e65fcd60916
Andrew Pollock <apollock@google.com>
2024-11-20 02:57:28 +0000

    fix(analysis): better tolerate GIT analysis crashes
    This commit discards any GIT events when known failures in analysis are
    seen, so that the rest of the record may still be imported successfully.
    
    When GIT range analysis crashes, the entire record is discarded. The
    record's `affected[]` may contain other non-GIT events (i.e. `package`
    entries) that will be of value.
    
    There are two known scenarios where GIT range analysis crashes:
    - the commit being analysed is an orphaned commit
    - the commit cannot be found in the repository (because it is from a
      fork)
    
    Includes a test to ensure that invalid GIT events are removed from
    records crashing analysis (and the record otherwise imports
    successfully)

--- osv/analyze_tool/__init__.py
+++ osv/analyze_tool/__init__.py
@@ -25,6 +25,7 @@ import yaml
 # pylint: disable=relative-beyond-top-level
 from .. import impact
 from .. import sources
+from .. import vulnerability_pb2
 
 
 def main():
@@ -98,11 +99,27 @@ def analyze(path, checkout_path, key_path, analyze_git, detect_cherrypicks):
     logging.error('Failed to parse %s', path)
     return
 
-  result = impact.analyze(
-      vuln,
-      analyze_git=analyze_git,
-      checkout_path=checkout_path,
-      detect_cherrypicks=detect_cherrypicks)
+  try:
+    result = impact.analyze(
+        vuln,
+        analyze_git=analyze_git,
+        checkout_path=checkout_path,
+        detect_cherrypicks=detect_cherrypicks)
+  except (IndexError, subprocess.CalledProcessError) as e:
+    if analyze_git:
+      logging.warning('Got "%s", retrying without the GIT events', e)
+      affected_without_git_events = [
+          a for a in vuln.affected
+          if any(r.type != vulnerability_pb2.Range.Type.GIT for r in a.ranges)
+      ]
+      del vuln.affected[:]
+      vuln.affected.extend(affected_without_git_events)
+      result = impact.analyze(
+          vuln,
+          analyze_git=analyze_git,
+          checkout_path=checkout_path,
+          detect_cherrypicks=detect_cherrypicks)
+
   if not result.has_changes:
     logging.info('No changes required.')
     return

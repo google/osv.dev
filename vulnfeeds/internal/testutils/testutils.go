d5715a6a3be4943c0f09eeed95477e21546cf37c
Michael Kedar <michaelkedar@google.com>
2025-05-22 13:49:45 +1000

    fix: remove illegal characters in testdata

--- vulnfeeds/internal/testutils/testutils.go
+++ vulnfeeds/internal/testutils/testutils.go
@@ -11,6 +11,31 @@ import (
 	"gopkg.in/dnaeon/go-vcr.v4/pkg/recorder"
 )
 
+// SetupVCR sets up and returns a go-vcr recorder for the current test.
+// It handles adding a Cleanup hook for the recorder.
+func SetupVCR(t *testing.T) *recorder.Recorder {
+	t.Helper()
+	cassetteName := t.Name()
+	// These characters seem to be illegal in filenames for go modproxy
+	// Strip replace them all with _ so this can actually be downloaded.
+	// https://cs.opensource.google/go/x/mod/+/master:module/module.go;l=278
+	const illegalChars = "\"'*/:;<>?\\`|"
+	for _, c := range illegalChars {
+		cassetteName = strings.ReplaceAll(cassetteName, string(c), "_")
+	}
+	r, err := recorder.New(filepath.Join("testdata", cassetteName))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := r.Stop(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	return r
+}
+
 // SetupGitVCR sets up go-vcr http & https capture and replay for go-git,
 // as well as the Cleanup for the recorder.
 //
@@ -19,12 +44,9 @@ import (
 //
 // Note: this only affects http/https protocols - git:// and ssh:// connections
 // cannot be routed through go-vcr.
-func SetupGitVCR(t *testing.T) {
+func SetupGitVCR(t *testing.T) *recorder.Recorder {
 	t.Helper()
-	r, err := recorder.New(filepath.Join("testdata", strings.ReplaceAll(t.Name(), "/", "_")))
-	if err != nil {
-		t.Fatal(err)
-	}
+	r := SetupVCR(t)
 	httpClient := r.GetDefaultClient()
 	client.InstallProtocol("http", githttp.NewClient(httpClient))
 	client.InstallProtocol("https", githttp.NewClient(httpClient))
@@ -33,8 +55,7 @@ func SetupGitVCR(t *testing.T) {
 		// Restore the protocols to their defaults in case another test needs them.
 		client.InstallProtocol("http", githttp.DefaultClient)
 		client.InstallProtocol("https", githttp.DefaultClient)
-		if err := r.Stop(); err != nil {
-			t.Error(err)
-		}
 	})
+
+	return r
 }

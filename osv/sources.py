31420ee602fe6de115e4cf32b4dea4d20c37ef92
Rex P <rexpan@google.com>
2025-05-27 10:15:05 +1000

    Remove unused import

--- osv/sources.py
+++ osv/sources.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Importer sources."""
-import datetime
 import json
 import hashlib
 import logging

53117d09091a7cf1897d134314aa66a387f01375
Rex P <rexpan@google.com>
2025-08-06 14:30:47 +1000

    Fix safe upload to actually upload a file

--- gcp/workers/exporter/exporter.py
+++ gcp/workers/exporter/exporter.py
@@ -232,6 +232,9 @@ def safe_upload_single(bucket: Bucket,
           f'with {source_path} ({source_size} bytes)'))
     if blob:
       blob.upload_from_filename(source_path, retry=retry.DEFAULT_RETRY)
+    else:
+      (bucket.blob(target_path).upload_from_filename(
+          source_path, retry=retry.DEFAULT_RETRY))
   except Exception as e:
     logging.exception('Failed to export: %s', e)
 

e69b7fb5e733c65cc20e5ffd9e22c5cb92e9fd82
Jess Lowe <jesslowe@google.com>
2025-09-09 04:00:00 +0000

    rename sortBadSemver in tests

--- vulnfeeds/cmd/cvelist2osv/version_extraction_test.go
+++ vulnfeeds/cmd/cvelist2osv/version_extraction_test.go
@@ -182,7 +182,7 @@ func TestFindNormalAffectedRanges(t *testing.T) {
 	}
 }
 
-func TestSortBadSemver(t *testing.T) {
+func TestCompareSemverLike(t *testing.T) {
 	tests := []struct {
 		name string
 		a, b string
@@ -197,8 +197,8 @@ func TestSortBadSemver(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := sortBadSemver(tt.a, tt.b); got != tt.want {
-				t.Errorf("sortBadSemver() = %v, want %v", got, tt.want)
+			if got := compareSemverLike(tt.a, tt.b); got != tt.want {
+				t.Errorf("compareSemverLike() = %v, want %v", got, tt.want)
 			}
 		})
 	}

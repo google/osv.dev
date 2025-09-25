bcd6685720df17686c01a93e9733ad02e1903afd
Jess Lowe <86962800+jess-lowe@users.noreply.github.com>
2025-09-26 08:24:23 +1000

    fix: check if there are no metrics on NVD before attempting to add to Alpine/Debian records (#4044)

--- vulnfeeds/cmd/alpine/main.go
+++ vulnfeeds/cmd/alpine/main.go
@@ -36,7 +36,7 @@ func main() {
 	logger.InitGlobalLogger()
 
 	alpineOutputPath := flag.String(
-		"alpineOutput",
+		"output_path",
 		alpineOutputPathDefault,
 		"path to output general alpine affected package information")
 	outputBucketName := flag.String("output_bucket", outputBucketDefault, "The GCS bucket to write to.")
@@ -220,7 +220,10 @@ func generateAlpineOSV(allAlpineSecDb map[string][]VersionAndPkg, allCVEs map[cv
 			logger.Warn(fmt.Sprintf("Skipping %s as no affected versions found.", v.ID), slog.String("cveID", cveID))
 			continue
 		}
-		v.AddSeverity(cve.CVE.Metrics)
+		if cve.CVE.Metrics != nil {
+			v.AddSeverity(cve.CVE.Metrics)
+		}
+
 		osvVulnerabilities = append(osvVulnerabilities, v)
 	}
 

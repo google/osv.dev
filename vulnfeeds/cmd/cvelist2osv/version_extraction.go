e3ff1b8f6d154c97959cd5c32aefc3939c08e95f
Jess Lowe <jesslowe@google.com>
2025-10-01 04:52:04 +0000

    Fix buildVersionRange accessibility

--- vulnfeeds/cmd/cvelist2osv/version_extraction.go
+++ vulnfeeds/cmd/cvelist2osv/version_extraction.go
@@ -236,9 +236,9 @@ func gitVersionsToCommits(cveID cves.CVEID, versionRanges []osvschema.Range, rep
 				var newVR osvschema.Range
 
 				if fixedCommit != "" {
-					newVR = buildVersionRange(introducedCommit, "", fixedCommit)
+					newVR = cves.BuildVersionRange(introducedCommit, "", fixedCommit)
 				} else {
-					newVR = buildVersionRange(introducedCommit, lastAffectedCommit, "")
+					newVR = cves.BuildVersionRange(introducedCommit, lastAffectedCommit, "")
 				}
 
 				newVR.Repo = repo
@@ -378,7 +378,7 @@ func findInverseAffectedRanges(cveAff cves.Affected, cnaAssigner string, metrics
 	// Create ranges by pairing sorted introduced and fixed versions.
 	for index, f := range fixed {
 		if index < len(introduced) {
-			ranges = append(ranges, buildVersionRange(introduced[index], "", f))
+			ranges = append(ranges, cves.BuildVersionRange(introduced[index], "", f))
 			metrics.Notes = append(metrics.Notes, "Introduced from version value - "+introduced[index])
 			metrics.Notes = append(metrics.Notes, "Fixed from version value - "+f)
 		}
@@ -454,10 +454,10 @@ func findNormalAffectedRanges(affected cves.Affected, metrics *ConversionMetrics
 				continue
 			}
 			if av.Fixed != "" {
-				versionRanges = append(versionRanges, buildVersionRange(av.Introduced, "", av.Fixed))
+				versionRanges = append(versionRanges, cves.BuildVersionRange(av.Introduced, "", av.Fixed))
 				continue
 			} else if av.LastAffected != "" {
-				versionRanges = append(versionRanges, buildVersionRange(av.Introduced, av.LastAffected, ""))
+				versionRanges = append(versionRanges, cves.BuildVersionRange(av.Introduced, av.LastAffected, ""))
 				continue
 			}
 		}
@@ -479,7 +479,7 @@ func findNormalAffectedRanges(affected cves.Affected, metrics *ConversionMetrics
 
 		// As a fallback, assume a single version means it's the last affected version.
 		if vQuality.AtLeast(acceptableQuality) {
-			versionRanges = append(versionRanges, buildVersionRange("0", vers.Version, ""))
+			versionRanges = append(versionRanges, cves.BuildVersionRange("0", vers.Version, ""))
 			metrics.Notes = append(metrics.Notes, fmt.Sprintf("%s - Single version found %v - Assuming introduced = 0 and last affected = %v", vQuality, vers.Version, vers.Version))
 		}
 	}
@@ -495,31 +495,6 @@ func findNormalAffectedRanges(affected cves.Affected, metrics *ConversionMetrics
 	}
 
 	return versionRanges, mostFrequentVersionType
-}
-
-// buildVersionRange is a helper function that adds 'introduced', 'fixed', or 'last_affected'
-// events to an OSV version range. If 'intro' is empty, it defaults to "0".
-func buildVersionRange(intro string, lastAff string, fixed string) osvschema.Range {
-	var versionRange osvschema.Range
-	var i string
-	if intro == "" {
-		i = "0"
-	} else {
-		i = intro
-	}
-	versionRange.Events = append(versionRange.Events, osvschema.Event{
-		Introduced: i})
-
-	if fixed != "" {
-		versionRange.Events = append(versionRange.Events, osvschema.Event{
-			Fixed: fixed})
-	} else if lastAff != "" {
-		versionRange.Events = append(versionRange.Events, osvschema.Event{
-			LastAffected: lastAff,
-		})
-	}
-
-	return versionRange
 }
 
 // compareSemverLike provides a custom comparison function for version strings that may not

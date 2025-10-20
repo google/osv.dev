9aca912f0852d360ef3216a8f826f1e577c23e00
Michael Kedar <michaelkedar@google.com>
2025-10-21 09:53:28 +1100

    uploadToGCS & README

--- go/cmd/exporter/exporter.go
+++ go/cmd/exporter/exporter.go
@@ -1,12 +1,3 @@
-// Package main runs the GCS exporter.
-// The exporter is responsible for creating many files in the OSV vulnerabilities bucket:
-// - [ECOSYSTEM]/VULN-ID.json - OSV JSON file for each vulnerability in each ecosystem
-// - [ECOSYSTEM]/all.zip - contains each OSV JSON file for that ecosystem
-// - [ECOSYSTEM]/modified_id.csv - contains the (modified, ID) of each vulnerability in the ecosystem directory
-// - /ecosystems.txt - a line-separated list of each exported ecosystem
-// - /all.zip - contains every OSV JSON file across all ecosytems
-// - /modified_id.csv - the (modified, [ECOSYSTEM]/ID) of every vulnerability across all ecosystem directories
-// - GIT/osv_git.json - a json array of every OSV vulnerability that has Vanir signatures.
 package main
 
 import (
@@ -32,9 +23,9 @@ const gcsProtoPrefix = "all/pb/"
 func main() {
 	logger.InitGlobalLogger()
 
-	outBucketName := flag.String("bucket", "osv-vulnerabilities", "Output bucket or directory name. If -local is true, this is a local path; otherwise, it's a GCS bucket name.")
+	outBucketName := flag.String("bucket", "osv-test-vulnerabilities", "Output bucket or directory name. If -local is true, this is a local path; otherwise, it's a GCS bucket name.")
 	vulnBucketName := flag.String("osv_vulns_bucket", os.Getenv("OSV_VULNERABILITIES_BUCKET"), "GCS bucket to read vulnerability protobufs from. Can also be set with the OSV_VULNERABILITIES_BUCKET environment variable.")
-	local := flag.Bool("local", true, "If true, writes the output to a local directory specified by -bucket instead of a GCS bucket.")
+	uploadToGCS := flag.Bool("uploadToGCS", false, "If false, writes the output to a local directory specified by -bucket instead of a GCS bucket.")
 	numWorkers := flag.Int("num_workers", 200, "The total number of concurrent workers to use for downloading from GCS and writing the output.")
 
 	flag.Parse()
@@ -42,7 +33,7 @@ func main() {
 	logger.Info("exporter starting",
 		slog.String("bucket", *outBucketName),
 		slog.String("osv_vulns_bucket", *vulnBucketName),
-		slog.Bool("local", *local),
+		slog.Bool("uploadToGCS", *uploadToGCS),
 		slog.Int("num_workers", *numWorkers))
 
 	if *vulnBucketName == "" {
@@ -60,10 +51,10 @@ func main() {
 	vulnBucket := cl.Bucket(*vulnBucketName)
 	var outBucket *storage.BucketHandle
 	var outPrefix string
-	if *local {
-		outPrefix = *outBucketName
-	} else {
+	if *uploadToGCS {
 		outBucket = cl.Bucket(*outBucketName)
+	} else {
+		outPrefix = *outBucketName
 	}
 
 	// The exporter uses a pipeline of channels and worker pools. The data flow is as follows:

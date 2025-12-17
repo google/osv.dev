52e044c57b505192f014c7e2b76d9c7a6a25d1cf
Michael Kedar <michaelkedar@google.com>
2025-12-18 10:20:05 +1100

    fix: Revert "chore: remove old python exporter and alias code" (#4511)
    Reverts google/osv.dev#4482
    The website emulator depends on the alias/upstream computation to
    populate the database / GCS. Trying to fix it is actually quite
    complicated, so I'm restoring the original alias/upstream code to
    unblock testing.

---
+++ gcp/workers/alias/upstream_computation.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python3
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""OSV Upstream relation computation."""
+
+from collections import defaultdict
+import datetime
+import json
+import logging
+
+from google.cloud import exceptions
+from google.cloud import ndb
+
+import osv
+import osv.logs
+from osv import gcs, pubsub
+
+
+def compute_upstream(target_bug, bugs: dict[str, set[str]]) -> list[str]:
+  """Computes all upstream vulnerabilities for the given bug ID.
+  The returned list contains all of the bug IDs that are upstream of the
+  target bug ID, including transitive upstreams."""
+  visited = set()
+
+  target_bug_upstream = target_bug
+  if not target_bug_upstream:
+    return []
+  to_visit = set(target_bug_upstream)
+  while to_visit:
+    bug_id = to_visit.pop()
+    if bug_id in visited:
+      continue
+    visited.add(bug_id)
+    upstreams = set()
+    if bug_id in bugs.keys():
+      bug = bugs.get(bug_id)
+      upstreams = set(bug)
+
+    to_visit.update(upstreams - visited)
+
+  # Returns a sorted list of bug IDs, which ensures deterministic behaviour
+  # and avoids unnecessary updates.
+  return sorted(visited)
+
+
+def _create_group(bug_id: str, upstream_ids: list[str]) -> osv.UpstreamGroup:
+  """Creates a new upstream group in the datastore."""
+
+  new_group = osv.UpstreamGroup(
+      id=bug_id,
+      db_id=bug_id,
+      upstream_ids=upstream_ids,
+      last_modified=datetime.datetime.now(datetime.UTC))
+  new_group.put()
+  _update_vuln_with_group(bug_id, new_group)
+
+  return new_group
+
+
+def _update_group(upstream_group: osv.UpstreamGroup,
+                  upstream_ids: list[str]) -> osv.UpstreamGroup | None:
+  """Updates the upstream group in the datastore."""
+  if len(upstream_ids) == 0:
+    logging.info('Deleting upstream group due to too few bugs: %s',
+                 upstream_ids)
+    upstream_group.key.delete()
+    _update_vuln_with_group(upstream_group.db_id, None)
+    return None
+
+  if upstream_ids == upstream_group.upstream_ids:
+    return None
+
+  upstream_group.upstream_ids = upstream_ids
+  upstream_group.last_modified = datetime.datetime.now(datetime.UTC)
+  upstream_group.put()
+  _update_vuln_with_group(upstream_group.db_id, upstream_group)
+  return upstream_group
+
+
+def _update_vuln_with_group(vuln_id: str, upstream: osv.UpstreamGroup | None):
+  """Updates the Vulnerability in Datastore & GCS with the new upstream group.
+  If `upstream` is None, assumes a preexisting UpstreamGroup was just deleted.
+  """
+  # Get the existing vulnerability first, so we can recalculate search_indices
+  result = gcs.get_by_id_with_generation(vuln_id)
+  if result is None:
+    logging.error('vulnerability not in GCS - %s', vuln_id)
+    pubsub.publish_failure(b'', type='gcs_missing', id=vuln_id)
+    return
+  vuln_proto, generation = result
+
+  def transaction():
+    vuln: osv.Vulnerability = osv.Vulnerability.get_by_id(vuln_id)
+    if vuln is None:
+      logging.error('vulnerability not in Datastore - %s', vuln_id)
+      # TODO(michaelkedar): What to do in this case?
+      return
+    if upstream is None:
+      modified = datetime.datetime.now(datetime.UTC)
+      upstream_group = []
+    else:
+      modified = upstream.last_modified
+      upstream_group = upstream.upstream_ids
+    vuln_proto.upstream[:] = upstream_group
+    vuln_proto.modified.FromDatetime(modified)
+    osv.ListedVulnerability.from_vulnerability(vuln_proto).put()
+    vuln.modified = modified
+    vuln.put()
+
+  ndb.transaction(transaction)
+  try:
+    gcs.upload_vulnerability(vuln_proto, generation)
+  except exceptions.PreconditionFailed:
+    logging.error('Generation mismatch when writing upstream for %s', vuln_id)
+    osv.pubsub.publish_failure(
+        b'', type='gcs_gen_mismatch', id=vuln_id, field='upstream')
+  except Exception:
+    logging.error('Writing to bucket failed for %s', vuln_id)
+    osv.pubsub.publish_failure(
+        vuln_proto.SerializeToString(deterministic=True), type='gcs_retry')
+
+
+def compute_upstream_hierarchy(
+    target_upstream_group: osv.UpstreamGroup,
+    all_upstream_groups: dict[str, osv.UpstreamGroup]) -> None:
+  """Computes all upstream vulnerabilities for the given bug ID.
+  The returned list contains all of the bug IDs that are upstream of the
+  target bug ID, including transitive upstreams in a map hierarchy.
+  upstream_group:
+        { db_id: bug id
+          upstream_ids: list of upstream bug ids
+          last_modified_date: date
+          upstream_hierarchy: JSON string of upstream hierarchy
+        }
+  """
+
+  # To convert to json, sets need to be converted to lists
+  # and sorting is done for a more consistent outcome.
+  def set_default(obj):
+    if isinstance(obj, set):
+      return list(sorted(obj))
+    raise TypeError
+
+  visited = set()
+  upstream_map = {}
+  to_visit = set([target_upstream_group.db_id])
+  # BFS navigation through the upstream hierarchy of a given upstream group
+  while to_visit:
+    bug_id = to_visit.pop()
+    if bug_id in visited:
+      continue
+    visited.add(bug_id)
+    upstream_group = all_upstream_groups.get(bug_id)
+    if upstream_group is None:
+      continue
+
+    upstreams = set(upstream_group.upstream_ids)
+    if not upstreams:
+      continue
+    for upstream in upstreams:
+      if upstream not in visited and upstream not in to_visit:
+        to_visit.add(upstream)
+      else:
+        if bug_id not in upstream_map:
+          upstream_map[bug_id] = set([upstream])
+        else:
+          upstream_map[bug_id].add(upstream)
+    # Add the immediate upstreams of the bug to the dict
+    upstream_map[bug_id] = upstreams
+    to_visit.update(upstreams - visited)
+
+  # Ensure there are no duplicate entries where transitive vulns appear
+  for k, v in upstream_map.items():
+    if k is target_upstream_group.db_id:
+      continue
+    upstream_map[target_upstream_group
+                 .db_id] = upstream_map[target_upstream_group.db_id] - v
+
+  # Update the datastore entry if hierarchy has changed
+  if upstream_map:
+    upstream_json = json.dumps(upstream_map, default=set_default)
+    if upstream_json == target_upstream_group.upstream_hierarchy:
+      return
+    target_upstream_group.upstream_hierarchy = upstream_json
+    target_upstream_group.put()
+
+
+def main():
+  """Updates all upstream groups in the datastore by re-computing existing
+  UpstreamGroups and creating new UpstreamGroups for un-computed bugs."""
+
+  # Query for all bugs that have upstreams.
+  updated_bugs = []
+  logging.info('Retrieving bugs...')
+  bugs_query = osv.Bug.query(osv.Bug.upstream_raw > '')
+
+  bugs = defaultdict(set)
+  for bug in bugs_query.iter(projection=[osv.Bug.db_id, osv.Bug.upstream_raw]):
+    bugs[bug.db_id].add(bug.upstream_raw[0])
+  logging.info('%s Bugs successfully retrieved', len(bugs))
+
+  logging.info('Retrieving upstream groups...')
+  upstream_groups = {
+      group.db_id: group for group in osv.UpstreamGroup.query().iter()
+  }
+  logging.info('Upstream Groups successfully retrieved')
+
+  for bug_id, bug in bugs.items():
+    # Get the specific upstream_group ID
+    upstream_group = upstream_groups.get(bug_id)
+    # Recompute the transitive upstreams and compare with the existing group
+    upstream_ids = compute_upstream(bug, bugs)
+    if upstream_group:
+      if upstream_ids == upstream_group.upstream_ids:
+        continue
+      # Update the existing UpstreamGroup
+      new_upstream_group = _update_group(upstream_group, upstream_ids)
+      if new_upstream_group is None:
+        continue
+      updated_bugs.append(new_upstream_group)
+      upstream_groups[bug_id] = new_upstream_group
+      logging.info('Upstream group updated for bug: %s', bug_id)
+    else:
+      # Create a new UpstreamGroup
+      new_upstream_group = _create_group(bug_id, upstream_ids)
+      logging.info('New upstream group created for bug: %s', bug_id)
+      updated_bugs.append(new_upstream_group)
+      upstream_groups[bug_id] = new_upstream_group
+
+  for group in updated_bugs:
+    # Recompute the upstream hierarchies
+    compute_upstream_hierarchy(group, upstream_groups)
+    logging.info('Upstream hierarchy updated for bug: %s', group.db_id)
+
+
+if __name__ == '__main__':
+  _ndb_client = ndb.Client()
+  osv.logs.setup_gcp_logging('upstream')
+  with _ndb_client.context():
+    main()
---
+++ gcp/workers/exporter/exporter.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python3
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""OSV Exporter."""
+import argparse
+import concurrent.futures
+import csv
+import json
+import logging
+import os
+import zipfile
+from typing import List
+
+from google.cloud import ndb
+from google.cloud import storage
+from google.cloud.storage import retry
+from google.cloud.storage.bucket import Bucket
+from google.protobuf import json_format
+
+import requests
+
+import osv
+import osv.logs
+
+DEFAULT_WORK_DIR = '/work'
+
+DEFAULT_EXPORT_BUCKET = 'osv-vulnerabilities'
+DEFAULT_SAFE_DELTA_PCT = 10
+_EXPORT_WORKERS = 32
+ECOSYSTEMS_FILE = 'ecosystems.txt'
+LAST_MODIFIED_FILE = 'modified_id.csv'
+VANIR_SIGNATURES_KEY = 'vanir_signatures'
+OSV_GIT_JSON_FILE_NAME = 'osv_git.json'
+
+
+class Error(Exception):
+  """Base exception class."""
+
+
+def modify_storage_client_adapters(storage_client: storage.Client,
+                                   pool_connections: int = 128,
+                                   max_retries: int = 3,
+                                   pool_block: bool = True) -> storage.Client:
+  """Returns a modified google.cloud.storage.Client object.
+
+  Due to many concurrent GCS connections, the default connection pool can become
+  overwhelmed, introducing delays.
+
+  Solution described in https://github.com/googleapis/python-storage/issues/253
+
+  These affect the urllib3.HTTPConnectionPool underpinning the storage.Client's
+  HTTP requests.
+
+  Args:
+    storage_client: an existing google.cloud.storage.Client object
+    pool_connections: number of pool_connections desired
+    max_retries: maximum retries
+    pool_block: blocking behaviour when pool is exhausted
+
+  Returns:
+    the google.cloud.storage.Client appropriately modified.
+
+  """
+  adapter = requests.adapters.HTTPAdapter(
+      pool_connections=pool_connections,
+      max_retries=max_retries,
+      pool_block=pool_block)
+  # pylint: disable=protected-access
+  storage_client._http.mount('https://', adapter)
+  storage_client._http._auth_request.session.mount('https://', adapter)
+  return storage_client
+
+
+class Exporter:
+  """Exporter."""
+
+  def __init__(self, work_dir, export_bucket, ecosystem):
+    self._work_dir = work_dir
+    self._export_bucket = export_bucket
+    self._ecosystem = ecosystem
+
+  def run(self):
+    """Run exporter."""
+    if self._ecosystem == "list":
+      query = osv.Bug.query(projection=[osv.Bug.ecosystem], distinct=True)
+      # Filter out ecosystems that contain a colon,
+      # as these represent Linux distro releases.
+      ecosystems = [
+          bug.ecosystem[0]
+          for bug in query
+          if bug.ecosystem and ':' not in bug.ecosystem[0]
+      ]
+      self._export_ecosystem_list_to_bucket(ecosystems, self._work_dir)
+    else:
+      self._export_ecosystem_to_bucket(self._ecosystem, self._work_dir)
+
+  def _export_ecosystem_list_to_bucket(self, ecosystems: List[str],
+                                       tmp_dir: str):
+    """Export an ecosystems.txt file with all of the ecosystem names.
+
+    See https://github.com/google/osv.dev/issues/619
+
+    Args:
+      ecosystems: the list of ecosystem names
+      tmp_dir: temporary directory for scratch
+    """
+
+    logging.info('Exporting ecosystem list to %s', ECOSYSTEMS_FILE)
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(self._export_bucket)
+    ecosystems_file_path = os.path.join(tmp_dir, ECOSYSTEMS_FILE)
+    with open(ecosystems_file_path, "w") as ecosystems_file:
+      ecosystems_file.writelines([e + "\n" for e in ecosystems])
+
+    upload_single(bucket, ecosystems_file_path, ECOSYSTEMS_FILE)
+
+  def _export_ecosystem_to_bucket(self, ecosystem: str, work_dir: str):
+    """Export the vulnerabilities in an ecosystem to GCS.
+
+    Args:
+      ecosystem: the ecosystem name
+      work_dir: working directory for scratch
+
+    This simultaneously exports every Bug for the given ecosystem to individual
+    files in the scratch filesystem, and a zip file in the scratch filesystem.
+
+    At the conclusion of this export, all of the files in the scratch filesystem
+    (including the zip file) are uploaded to the GCS bucket.
+    """
+    logging.info('Exporting vulnerabilities for ecosystem %s', ecosystem)
+    storage_client = modify_storage_client_adapters(storage.Client())
+    bucket = storage_client.get_bucket(self._export_bucket)
+
+    ecosystem_dir = os.path.join(work_dir, ecosystem)
+    os.makedirs(ecosystem_dir, exist_ok=True)
+    zip_path = os.path.join(ecosystem_dir, 'all.zip')
+    git_vulns_with_vanir_signatures = []
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+      files_to_zip = []
+      id_and_modified = []
+
+      @ndb.tasklet
+      def _export_to_file_and_zipfile(bug: osv.Bug):
+        """Write out a bug record to both a single file and the zip file."""
+        if not bug.public or bug.status == osv.BugStatus.UNPROCESSED:
+          return
+
+        try:
+          file_path = os.path.join(ecosystem_dir, bug.id() + '.json')
+          vulnerability = yield bug.to_vulnerability_async(
+              include_source=True, include_alias=True, include_upstream=True)
+          osv.write_vulnerability(vulnerability, file_path)
+
+          if ecosystem == 'GIT':
+            # The GIT ecosystem has vulnerabilities that have Vanir sigantures
+            # generated. These are exported to a file.
+            if any(VANIR_SIGNATURES_KEY in affected.database_specific
+                   for affected in vulnerability.affected):
+              git_vulns_with_vanir_signatures.append(
+                  json_format.MessageToDict(
+                      vulnerability, preserving_proto_field_name=True))
+
+          files_to_zip.append(file_path)
+          # ToJsonString converts it into an ISO string
+          # with timezone Z correctly appended
+          id_and_modified.append(
+              (vulnerability.modified.ToJsonString(), vulnerability.id))
+        except Exception:
+          logging.exception('Failed to export bug: "%s"', bug.id())
+          raise
+
+      # This *should* pause here until
+      # all the exports have been written to disk.
+      osv.Bug.query(
+          osv.Bug.ecosystem == ecosystem).map(_export_to_file_and_zipfile)
+
+      # Write out the GIT vulnerabilities that have Vanir signatures to
+      # a JSON file.
+      if ecosystem == 'GIT' and git_vulns_with_vanir_signatures:
+        output_path = os.path.join(ecosystem_dir, OSV_GIT_JSON_FILE_NAME)
+        with open(output_path, 'w') as f:
+          json.dump(git_vulns_with_vanir_signatures, f, indent=2)
+
+      files_to_zip.sort()
+      for file_path in files_to_zip:
+        zip_file.write(file_path, os.path.basename(file_path))
+
+      id_and_modified.sort(reverse=True)
+      with open(os.path.join(ecosystem_dir, LAST_MODIFIED_FILE),
+                'w') as modified_file:
+        csv.writer(modified_file).writerows(id_and_modified)
+
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=_EXPORT_WORKERS) as executor:
+      # Note: the individual ecosystem all.zip is included here
+      # TODO: use safe_upload_single() on the zip files.
+      for filename in os.listdir(ecosystem_dir):
+        executor.submit(upload_single, bucket,
+                        os.path.join(ecosystem_dir, filename),
+                        f'{ecosystem}/{filename}')
+
+
+def upload_single(bucket: Bucket, source_path: str, target_path: str):
+  """Upload a single file to a GCS bucket."""
+  logging.info('Uploading %s', target_path)
+  try:
+    blob = bucket.blob(target_path)
+    blob.upload_from_filename(source_path, retry=retry.DEFAULT_RETRY)
+  except Exception as e:
+    logging.exception('Failed to export: %s', e)
+
+
+def safe_upload_single(bucket: Bucket,
+                       source_path: str,
+                       target_path: str,
+                       safe_delta_pct: int = DEFAULT_SAFE_DELTA_PCT):
+  """Upload a single file to a GCS bucket, with a size check.
+
+  This refuses to overwrite the GCS object if the file size difference is
+  greater than the permitted threshold (10% by default).
+
+  NOTE: this intentionally only catches unexpectedly smaller files, not larger
+  ones.
+
+  Args:
+    bucket: (Bucket): the GCS bucket object to upload to.
+    source_path: (str): the source path to the file to upload.
+    target_path: (str): the target path in Bucket to upload to.
+    safe_delta_pct: (int): the threshold at which to raise an exception.
+
+  Raises:
+    Error: if safe_delta_pct is exceeded
+  """
+
+  source_size = os.stat(source_path).st_size
+  logging.info('Uploading %s', target_path)
+  try:
+    blob = bucket.get_blob(target_path)
+    if blob and blob.size and (source_size / blob.size) * 100 < safe_delta_pct:
+      raise (Error(
+          f'Cowardly refusing to overwrite {blob.name} ({blob.size} bytes) '
+          f'with {source_path} ({source_size} bytes)'))
+    if blob:
+      blob.upload_from_filename(source_path, retry=retry.DEFAULT_RETRY)
+    else:
+      bucket.blob(target_path).upload_from_filename(
+          source_path, retry=retry.DEFAULT_RETRY)
+  except Exception as e:
+    logging.exception('Failed to export: %s', e)
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Exporter')
+  parser.add_argument(
+      '--work_dir', help='Working directory', default=DEFAULT_WORK_DIR)
+  parser.add_argument(
+      '--bucket',
+      help='Bucket name to export to',
+      default=DEFAULT_EXPORT_BUCKET)
+  parser.add_argument(
+      '--ecosystem',
+      required=True,
+      help='Ecosystem to upload, pass the value "list" ' +
+      'to export the ecosystem.txt file')
+  args = parser.parse_args()
+
+  exporter = Exporter(args.work_dir, args.bucket, args.ecosystem)
+  exporter.run()
+
+
+if __name__ == '__main__':
+  _ndb_client = ndb.Client()
+  osv.logs.setup_gcp_logging('exporter')
+  with _ndb_client.context():
+    main()

49a32d99f2e4f1b5550347b7aa0a592356e331ff
Rex P <rexpan@google.com>
2025-12-23 14:33:47 +1100

    Fix lints

--- osv/impact.py
+++ osv/impact.py
@@ -16,6 +16,7 @@
 import codecs
 from dataclasses import dataclass
 import logging
+import hashlib
 import os
 import subprocess
 import tempfile
@@ -656,11 +657,11 @@ def _analyze_git_ranges(repo_analyzer: RepoAnalyzer, checkout_path: str,
     # We'd prefer to only download the commit history, but detect_cherrypicks
     # requires having the blobs available to function correctly.
     blobless = not repo_analyzer.detect_cherrypicks
-    repo_name = os.path.basename(
-          affected_range.repo.rstrip('/')).rstrip('.git')
+    repo_name = os.path.basename(affected_range.repo.rstrip('/')).rstrip('.git')
     if checkout_path:
       # Repo name is <base-name>-<sha256(affected_range.repo)>.
-      clone_repo_dir = f'{repo_name}-{hashlib.sha256(affected_range.repo.encode()).hexdigest()}'
+      repo_hash = hashlib.sha256(affected_range.repo.encode()).hexdigest()
+      clone_repo_dir = f'{repo_name}-{repo_hash}'
       package_repo = repos.ensure_updated_checkout(
           affected_range.repo,
           os.path.join(checkout_path, clone_repo_dir),

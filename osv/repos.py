6040b6747a3e8a7be3193ed779a79f86d9a925c3
Michael Kedar <michaelkedar@google.com>
2026-01-08 15:51:09 +1100

    Update osv/repos.py

--- osv/repos.py
+++ osv/repos.py
@@ -117,7 +117,7 @@ class RepoInaccessibleError(Exception):
 
 def clone(git_url, checkout_dir, git_callbacks=None, blobless=False):
   """Perform a clone."""
-  # Don't user Gitter for oss-fuzz-vulns repo because we may need to push to it
+  # Don't user Gitter for oss-fuzz-vulns repo because it requires auth
   if GITTER_HOST and git_url != 'ssh://github.com/google/oss-fuzz-vulns':
     try:
       os.makedirs(checkout_dir, exist_ok=True)

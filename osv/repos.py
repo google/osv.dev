9164acbf95e8c007dd023044830e665a9f9079c2
Rex P <rexpan@google.com>
2026-01-13 16:27:00 +1100

    Actually log errors as well

--- osv/repos.py
+++ osv/repos.py
@@ -145,6 +145,9 @@ def clone(git_url, checkout_dir, git_callbacks=None, blobless=False):
         os.remove(f'{checkout_dir}.zst')
 
         return pygit2.Repository(checkout_dir)
+      else:
+        logging.error(f'Failed to clone repo through gitter, '
+                      f'status: {resp.status_code}, response: {resp.text}')
     except requests.RequestException as e:
       raise GitCloneError(f'Failed to clone repo: {e}') from e
     except subprocess.CalledProcessError as e:

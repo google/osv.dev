f9090c51345d89700f45d0f2e6e99f81f81a8547
Jess Lowe <jesslowe@google.com>
2026-01-15 23:52:30 +0000

    fix lint

--- osv/repos.py
+++ osv/repos.py
@@ -114,12 +114,14 @@ class NoBranchError(Exception):
 class RepoInaccessibleError(Exception):
   """Git repository cannot be cloned due to being deleted or requiring auth."""
 
+
 def checkout_repo(checkout_dir):
   try:
     repo = pygit2.Repository(checkout_dir)
   except:
     repo = pygit2.Repository(checkout_dir, pygit2.enums.RepositoryOpenFlag.BARE)
   return repo
+
 
 def clone(git_url, checkout_dir, git_callbacks=None, blobless=False):
   """Perform a clone."""

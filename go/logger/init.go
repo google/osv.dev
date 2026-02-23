2b76d6f0d82b6b5bb87e2a2902f43b63818a249b
Joey L <joeylauy@google.com>
2026-02-23 02:04:41 +0000

    fix local logging after rebase

--- go/logger/init.go
+++ go/logger/init.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-	slogLogger = slog.New(newLocalHandler(os.Stdout))
+	slogLogger = slog.New(&contextHandler{newLocalHandler(os.Stdout)})
 	once       sync.Once
 	tp         *sdktrace.TracerProvider
 	projectID  string

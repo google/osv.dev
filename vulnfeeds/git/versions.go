4ab278d9ba54ed6a05a034a98a9258c7d283acdc
Jess Lowe <86962800+jess-lowe@users.noreply.github.com>
2026-03-11 16:08:13 +1100

    fix: too many logs about version being empty

--- vulnfeeds/git/versions.go
+++ vulnfeeds/git/versions.go
@@ -84,7 +84,7 @@ func VersionToAffectedCommit(version string, repo string, commitType models.Comm
 // Take an unnormalized version string, the pre-normalized mapping of tags to commits and return a commit hash.
 func VersionToCommit(version string, normalizedTags map[string]NormalizedTag) (string, error) {
 	if version == "" {
-		return "", errors.New("version cannot be empty")
+		return "", nil
 	}
 	// TODO: try unnormalized version first.
 	normalizedVersion, err := NormalizeVersion(version)

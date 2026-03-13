8c92e913c94110796ed6c12398e604362da84d86
Rex P <rexpan@google.com>
2026-03-13 17:15:22 +1100

    Change the task around

--- go/cmd/vuln-modified-sync/main.go
+++ go/cmd/vuln-modified-sync/main.go
@@ -5,24 +5,24 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
 	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
-
 
 	"cloud.google.com/go/datastore"
 	db "github.com/google/osv.dev/go/internal/database/datastore"
 	"github.com/google/osv.dev/go/logger"
 	"github.com/tidwall/gjson"
-	"google.golang.org/api/iterator"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
-type ValidationTask struct {
-	Vuln *db.Vulnerability
-	Key  *datastore.Key
+type SyncTask struct {
+	FilePath string
+	VulnID   string
 }
 
 func main() {
@@ -40,7 +40,7 @@ func main() {
 	project := os.Getenv("GOOGLE_CLOUD_PROJECT")
 	if project == "" {
 		// Use default test project for local testing if not set
-		project = "osv-vulnerabilities" 
+		project = "oss-vdb-test"
 	}
 
 	datastoreClient, err := datastore.NewClient(ctx, project)
@@ -49,7 +49,7 @@ func main() {
 	}
 	defer datastoreClient.Close()
 
-	taskCh := make(chan ValidationTask, *numWorkers)
+	taskCh := make(chan SyncTask, *numWorkers)
 	var wg sync.WaitGroup
 
 	logger.InfoContext(ctx, "Starting vulnerability modified sync...",
@@ -63,118 +63,116 @@ func main() {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			worker(ctx, taskCh, datastoreClient, *dir, *dryRun)
+			worker(ctx, taskCh, datastoreClient, *dryRun)
 		}()
 	}
 
-	// Query datastore
-	q := datastore.NewQuery("Vulnerability")
-	it := datastoreClient.Run(ctx, q)
 	processed := 0
-	
-	for {
-		var vuln db.Vulnerability
-		key, err := it.Next(&vuln)
-		if err == iterator.Done {
-			break
-		}
+
+	err = filepath.WalkDir(*dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
-			logger.ErrorContext(ctx, "Failed to fetch vulnerability", slog.Any("error", err))
-			continue
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		
+		ext := filepath.Ext(path)
+		if ext != ".json" && ext != ".yaml" {
+			return nil
 		}
 
+		filename := filepath.Base(path)
+		vulnID := strings.TrimSuffix(filename, ext)
+
 		processed++
-		taskCh <- ValidationTask{
-			Vuln: &vuln,
-			Key:  key,
+		taskCh <- SyncTask{
+			FilePath: path,
+			VulnID:   vulnID,
 		}
+		return nil
+	})
+
+	if err != nil {
+		logger.ErrorContext(ctx, "Failed to walk directory", slog.Any("error", err), slog.String("dir", *dir))
 	}
 
 	close(taskCh)
 	wg.Wait()
 
-	logger.InfoContext(ctx, "Sync completed", slog.Int("processed_records", processed))
+	logger.InfoContext(ctx, "Sync completed", slog.Int("processed_files", processed))
 }
 
-func worker(ctx context.Context, taskCh <-chan ValidationTask, client *datastore.Client, dir string, dryRun bool) {
+func worker(ctx context.Context, taskCh <-chan SyncTask, client *datastore.Client, dryRun bool) {
 	for task := range taskCh {
-		checkAndUpdate(ctx, client, task, dir, dryRun)
+		checkAndUpdate(ctx, client, task, dryRun)
 	}
 }
 
-func checkAndUpdate(ctx context.Context, client *datastore.Client, task ValidationTask, dir string, dryRun bool) {
-	vulnID := task.Key.Name
-	
-	// Try JSON first, then YAML
-	filePath := filepath.Join(dir, vulnID+".json")
-	format := "json"
-	
-	file, err := os.Open(filePath)
+func checkAndUpdate(ctx context.Context, client *datastore.Client, task SyncTask, dryRun bool) {
+	file, err := os.Open(task.FilePath)
 	if err != nil {
-		if os.IsNotExist(err) {
-			filePath = filepath.Join(dir, vulnID+".yaml")
-			format = "yaml"
-			file, err = os.Open(filePath)
-			if err != nil {
-				if os.IsNotExist(err) {
-					// File does not exist, ignore
-					return
-				}
-				logger.ErrorContext(ctx, "Failed to open yaml file", slog.Any("error", err), slog.String("file", filePath))
-				return
-			}
-		} else {
-			logger.ErrorContext(ctx, "Failed to open json file", slog.Any("error", err), slog.String("file", filePath))
-			return
-		}
+		logger.ErrorContext(ctx, "Failed to open file", slog.Any("error", err), slog.String("file", task.FilePath))
+		return
 	}
 	defer file.Close()
 
 	data, err := io.ReadAll(file)
 	if err != nil {
-		logger.ErrorContext(ctx, "Failed to read file", slog.Any("error", err), slog.String("file", filePath))
+		logger.ErrorContext(ctx, "Failed to read file", slog.Any("error", err), slog.String("file", task.FilePath))
 		return
 	}
 
-	if format == "yaml" {
+	if filepath.Ext(task.FilePath) == ".yaml" {
 		jsonData, err := yaml.ToJSON(data)
 		if err != nil {
 			logger.ErrorContext(ctx, "Failed to convert YAML to JSON",
 				slog.Any("error", err),
-				slog.String("path", filePath))
+				slog.String("path", task.FilePath))
 			return
 		}
 		data = jsonData
 	}
 
 	res := gjson.GetBytes(data, "modified")
-
-	if (!res.Exists()) {
+	if !res.Exists() {
 		return
 	}
 
-	// Parse the modified time
-	modifiedTime, err := parseTime(res.String())
+	// Parse the modified time from file
+	fileModifiedTime, err := parseTime(res.String())
 	if err != nil {
-		logger.ErrorContext(ctx, "Failed to parse modified time from file", slog.Any("error", err), slog.String("file", filePath), slog.String("time_str", res.String()))
+		logger.ErrorContext(ctx, "Failed to parse modified time from file", slog.Any("error", err), slog.String("file", task.FilePath), slog.String("time_str", res.String()))
 		return
 	}
 
-	if modifiedTime.After(task.Vuln.Modified) {
-		logger.InfoContext(ctx, "File modified time is newer", 
-			slog.String("id", vulnID), 
-			slog.Time("datastore_modified", task.Vuln.Modified), 
-			slog.Time("file_modified", modifiedTime))
-			
+	// Fetch vulnerability from Datastore
+	key := datastore.NameKey("Vulnerability", task.VulnID, nil)
+	var vuln db.Vulnerability
+	if err := client.Get(ctx, key, &vuln); err != nil {
+		if err == datastore.ErrNoSuchEntity {
+			logger.InfoContext(ctx, "Vulnerability not found in datastore", slog.String("id", task.VulnID))
+		} else {
+			logger.ErrorContext(ctx, "Failed to get Vulnerability from datastore", slog.Any("error", err), slog.String("id", task.VulnID))
+		}
+		return
+	}
+
+	if fileModifiedTime.After(vuln.Modified) {
+		logger.InfoContext(ctx, "File modified time is newer",
+			slog.String("id", task.VulnID),
+			slog.Time("datastore_modified", vuln.Modified),
+			slog.Time("file_modified", fileModifiedTime))
+
 		if !dryRun {
 			// Update Datastore
-			task.Vuln.Modified = modifiedTime
-			task.Vuln.ModifiedRaw = modifiedTime
-			
-			if _, err := client.Put(ctx, task.Key, task.Vuln); err != nil {
-				logger.ErrorContext(ctx, "Failed to update Vulnerability", slog.Any("error", err), slog.String("id", vulnID))
+			vuln.Modified = fileModifiedTime
+			vuln.ModifiedRaw = fileModifiedTime
+
+			if _, err := client.Put(ctx, key, &vuln); err != nil {
+				logger.ErrorContext(ctx, "Failed to update Vulnerability", slog.Any("error", err), slog.String("id", task.VulnID))
 			} else {
-				logger.InfoContext(ctx, "Successfully updated datastore", slog.String("id", vulnID))
+				logger.InfoContext(ctx, "Successfully updated datastore", slog.String("id", task.VulnID))
 			}
 		}
 	}

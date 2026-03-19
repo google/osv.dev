c3fc47187c3c43f65bdf54d920ffa8242e688ac7
Rex P <106129829+another-rex@users.noreply.github.com>
2026-03-19 13:24:10 +1100

    feat: A reconciler to actually keep our records in check (#4991)
    Wrote a reconciler so that we catch any missing records within 30 hours
    of it happening.
    
    This acts like a last resort to help us identify leaks in our ingestion
    pipeline, and ideally should not be actually doing any reimporting.
    
    TODO: We might need to keep a list in the datastore of known bad entries
    so we avoid erroring on them constantly.

--- go/cmd/importer/main.go
+++ go/cmd/importer/main.go
@@ -36,9 +36,11 @@ func main() {
 	strictValidation := flag.Bool("strict-validation", false, "Do not import entries if they fail validation. "+
 		"Note: this only applies to SourceRepositories with strict_validation=true")
 	runDelete := flag.Bool("delete", false, "Bypass importing and propagate record deletions from source to Datastore")
+	runReconcile := flag.Bool("reconcile", false, "Analyze sources and trigger an import if the source modified date is newer than the database record")
 	deleteThresholdPct := flag.Float64("delete-threshold-pct", 10.0, "More than this percent of records for a given source being deleted triggers an error")
 	workDir := flag.String("work-dir", "/work", "Work directory for git repos")
 	numWorkers := flag.Int("num-workers", 50, "Number of workers to use for importing")
+	dryRun := flag.Bool("dry-run", false, "Do not send anything to the message bus if true")
 
 	flag.Parse()
 
@@ -53,6 +55,7 @@ func main() {
 		NumWorkers:       *numWorkers,
 		GitWorkDir:       filepath.Join(*workDir, "sources"),
 		SampleRate:       vulnerabilitySampleRate(),
+		DryRun:           *dryRun,
 	}
 
 	httpClient := retryablehttp.NewClient()
@@ -64,6 +67,13 @@ func main() {
 
 	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	go func() {
+		<-ctx.Done()
+		logger.InfoContext(ctx, "Context terminated, shutting down...")
+		time.Sleep(30 * time.Second)
+		logger.FatalContext(ctx, "Forced shut down after 30 seconds")
+	}()
 
 	datastoreClient, err := datastore.NewClient(ctx, project)
 	if err != nil {
@@ -91,6 +101,11 @@ func main() {
 			logger.FatalContext(ctx, "Importer-deleter failed", slog.Any("error", err))
 		}
 		logger.InfoContext(ctx, "Importer-deleter completed successfully")
+	} else if *runReconcile {
+		if err := importer.RunReconcile(ctx, config); err != nil {
+			logger.FatalContext(ctx, "Importer-reconciler failed", slog.Any("error", err))
+		}
+		logger.InfoContext(ctx, "Importer-reconciler completed successfully")
 	} else {
 		if err := importer.Run(ctx, config); err != nil {
 			logger.FatalContext(ctx, "Importer failed", slog.Any("error", err))

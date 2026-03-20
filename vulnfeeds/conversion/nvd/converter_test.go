24369ea884307961f2cba3e2c67c4e27e114b4b4
Jess Lowe <jesslowe@google.com>
2026-03-20 05:11:09 +0000

    remove unneeded test as output happens elsewhere

--- vulnfeeds/conversion/nvd/converter_test.go
+++ vulnfeeds/conversion/nvd/converter_test.go
@@ -8,12 +8,8 @@ import (
 
 	"github.com/go-git/go-git/v5/plumbing/transport/client"
 	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
-	"github.com/google/go-cmp/cmp"
 	"github.com/google/osv/vulnfeeds/git"
 	"github.com/google/osv/vulnfeeds/models"
-	"github.com/ossf/osv-schema/bindings/go/osvschema"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/testing/protocmp"
 )
 
 type roundTripperFunc func(*http.Request) (*http.Response, error)
@@ -89,64 +85,6 @@ func TestCVEToOSV_429(t *testing.T) {
 		})
 		if err != nil {
 			t.Fatal(err)
-		}
-	}
-}
-
-func TestCVEToOSV_ReferencesDeterminism(t *testing.T) {
-	cve := models.NVDCVE{
-		ID: "CVE-2025-12345",
-		References: []models.Reference{
-			{URL: "https://example.com/D"},
-			{URL: "https://example.com/A"},
-			{URL: "https://example.com/C", Tags: []string{"Patch"}},
-			{URL: "https://example.com/C"},
-			{URL: "https://example.com/B", Tags: []string{"Issue Tracking"}},
-			{URL: "https://example.com/E"},
-		},
-		Metrics: &models.CVEItemMetrics{},
-	}
-	metrics := &models.ConversionMetrics{}
-	outDir := t.TempDir()
-
-	var firstResult []*osvschema.Reference
-	for i := range 10 {
-		cache := &git.RepoTagsCache{}
-		CVEToOSV(cve, nil, cache, outDir, metrics, false, false)
-
-		var b []byte
-		err := filepath.Walk(outDir, func(path string, info os.FileInfo, _ error) error {
-			if !info.IsDir() && filepath.Ext(path) == ".json" {
-				var fileErr error
-				b, fileErr = os.ReadFile(path)
-				if fileErr != nil {
-					return fileErr
-				}
-			}
-
-			return nil
-		})
-		if err != nil {
-			t.Fatalf("Failed to walk or read OSV file: %v", err)
-		}
-
-		if len(b) == 0 {
-			t.Fatalf("Failed to find OSV file")
-		}
-
-		var vuln osvschema.Vulnerability
-		err = protojson.Unmarshal(b, &vuln)
-		if err != nil {
-			t.Fatalf("Failed to unmarshal OSV: %v", err)
-		}
-
-		if i == 0 {
-			firstResult = vuln.GetReferences()
-			continue
-		}
-
-		if diff := cmp.Diff(firstResult, vuln.GetReferences(), protocmp.Transform()); diff != "" {
-			t.Fatalf("Iteration %d produced different references result:\n%s", i, diff)
 		}
 	}
 }

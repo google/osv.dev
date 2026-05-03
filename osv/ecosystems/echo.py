6a3c96e2277df98c8493af079aea21cbb4be562d
Ori Zerah <ori.zerah@echohq.com>
2026-05-03 13:48:30 +0300

    refactor(echo): delegate versioning to PyPI/DPKG helpers
    Per review feedback, replace duplicated sort/coarse logic in the Echo
    ecosystem with delegation to the existing PyPI and DPKG helpers. Mirror
    the same change in the Go implementation by switching the Echo factory
    to dispatch on the suffix.

--- osv/ecosystems/echo.py
+++ osv/ecosystems/echo.py
@@ -13,17 +13,9 @@
 # limitations under the License.
 """Echo ecosystem helper."""
 
-import re
-from itertools import batched
-import packaging_legacy.version
-
-from ..third_party.univers.debian import Version as DebianVersion
-from .ecosystems_base import (
-    coarse_version_generic,
-    coarse_version_from_ints,
-    MAX_COARSE_PART,
-    OrderedEcosystem,
-)
+from .debian import DPKG
+from .ecosystems_base import OrderedEcosystem
+from .pypi import PyPI
 
 
 class Echo(OrderedEcosystem):
@@ -34,64 +26,13 @@ class Echo(OrderedEcosystem):
   - Echo:PyPI   - Python packages (PyPI/PEP 440 versioning)
   """
 
-  def _sort_key(self, version: str):
+  def _delegate(self) -> OrderedEcosystem:
     if self.suffix and self.suffix.lower() == 'pypi':
-      return packaging_legacy.version.parse(version)
-    if not DebianVersion.is_valid(version):
-      raise ValueError(f'Invalid version: {version}')
-    return DebianVersion.from_string(version)
+      return PyPI()
+    return DPKG()
+
+  def _sort_key(self, version: str):
+    return self._delegate()._sort_key(version)  # pylint: disable=protected-access
 
   def coarse_version(self, version: str) -> str:
-    if self.suffix and self.suffix.lower() == 'pypi':
-      return self._pypi_coarse_version(version)
-    return self._dpkg_coarse_version(version)
-
-  def _pypi_coarse_version(self, version: str) -> str:
-    """Coarse version using PyPI/PEP 440 semantics."""
-    ver = packaging_legacy.version.parse(version)
-    if isinstance(ver, packaging_legacy.version.LegacyVersion):
-      return coarse_version_from_ints([0])
-
-    epoch = ver.epoch
-    if version[0].lower() == 'v':
-      version = version[1:]
-    epochless = version.split('!', 1)[-1]
-
-    return coarse_version_generic(
-        epochless,
-        separators_regex=r'[.]',
-        truncate_regex=r'[+_-]',
-        implicit_split=True,
-        empty_as=None,
-        epoch=epoch)
-
-  def _dpkg_coarse_version(self, version: str) -> str:
-    """Coarse version using Debian dpkg semantics."""
-    if not DebianVersion.is_valid(version):
-      raise ValueError(f'Invalid version: {version}')
-
-    e, p, v = version.partition(':')
-    if not p:
-      v = e
-      e = '0'
-    try:
-      epoch = int(e)
-    except ValueError as exc:
-      raise ValueError(f'Invalid version: {version}') from exc
-
-    parts = re.findall(r'^$|\d+|\D+', v)
-    int_parts = []
-    for couple in batched(parts, 2):
-      if not couple[0].isdecimal():
-        break
-      int_parts.append(int(couple[0]))
-      if len(couple) == 1:
-        break
-      sep = couple[1]
-      if sep == '.':
-        continue
-      if sep[0] == '.':
-        int_parts.append(MAX_COARSE_PART + 1)
-      break
-
-    return coarse_version_from_ints(int_parts, epoch=epoch)
+    return self._delegate().coarse_version(version)

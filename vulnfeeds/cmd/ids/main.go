22d9bfef81c0fd2e7dd94b25b3837a2328bc4ed0
Jess Lowe <jesslowe@google.com>
2026-05-25 01:14:03 +0000

    copy data back so it's easier to migrate

---
+++ vulnfeeds/cmd/ids/main.go
@@ -0,0 +1,244 @@
+// package main contains a utility for assigning IDs to OSV records.
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/goccy/go-yaml"
+	"github.com/google/osv/vulnfeeds/utility/logger"
+	"github.com/google/osv/vulnfeeds/vulns"
+	"github.com/ossf/osv-schema/bindings/go/osvschema"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+const (
+	conflictFile       = ".id-allocator"
+	conflictMarkerSize = 32
+)
+
+type fileFormat string
+
+const (
+	fileFormatJSON = fileFormat("json")
+	fileFormatYAML = fileFormat("yaml")
+)
+
+var (
+	validFormats      = []fileFormat{fileFormatYAML, fileFormatJSON}
+	formatToExtension = map[fileFormat]string{
+		fileFormatYAML: ".yaml",
+		fileFormatJSON: ".json",
+	}
+)
+
+func main() {
+	prefix := flag.String("prefix", "", "Vulnerability prefix (e.g. \"PYSEC\".")
+	dir := flag.String("dir", "", "Path to vulnerabilities.")
+	format := flag.String("format", string(fileFormatYAML), "Format of OSV reports in the repository. Must be \"json\" or \"yaml\".")
+
+	flag.Parse()
+
+	logger.InitGlobalLogger()
+	defer logger.Close()
+
+	if *prefix == "" || *dir == "" {
+		flag.Usage()
+		return
+	}
+
+	if !slices.Contains(validFormats, fileFormat(*format)) {
+		flag.Usage()
+		return
+	}
+
+	if err := assignIDs(*prefix, *dir, fileFormat(*format)); err != nil {
+		logger.Info("Failed to assign IDs", slog.Any("err", err))
+		logger.Close() // os.Exit() doesn't call deferred functions
+		os.Exit(1)     //nolint:gocritic
+	}
+}
+
+func extractYearAndNum(prefix, filename string, format fileFormat) (int, int) {
+	// Extract year and num from "PREFIX-YEAR-NUM"
+	parts := strings.Split(strings.TrimSuffix(filename, formatToExtension[format]), "-")
+	if len(parts) != 3 {
+		return 0, 0
+	}
+
+	if parts[0] != prefix {
+		return 0, 0
+	}
+
+	year, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, 0
+	}
+
+	num, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return 0, 0
+	}
+
+	return year, num
+}
+
+func isUnassigned(prefix, filename string) bool {
+	return strings.HasPrefix(filename, prefix+"-0000-")
+}
+
+func assignID(prefix, path string, format fileFormat, yearCounters map[int]int, defaultYear int) error {
+	// Parse the existing vulnerability.
+	readf, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", path, err)
+	}
+	defer readf.Close()
+
+	vuln, err := readVulnWithFormat(readf, format)
+	if err != nil {
+		return fmt.Errorf("failed to parse %s: %w", format, err)
+	}
+	readf.Close()
+
+	// If the vulnerability has a published date, use the year from that.
+	// Otherwise, just default to the current year.
+	year := defaultYear
+	if vuln.GetPublished() != nil {
+		year = vuln.GetPublished().AsTime().Year()
+	}
+
+	// Allocate a new ID and write the new file.
+	id := yearCounters[year] + 1
+	yearCounters[year] = id
+
+	vuln.Id = fmt.Sprintf("%s-%d-%d", prefix, year, id)
+	newPath := filepath.Join(filepath.Dir(path), vuln.GetId()+formatToExtension[format])
+
+	writef, err := os.Create(newPath)
+	if err != nil {
+		return fmt.Errorf("failed to create %s: %w", newPath, err)
+	}
+	defer writef.Close()
+
+	if err := writeVulnWithFormat(vuln.Vulnerability, writef, format); err != nil {
+		return fmt.Errorf("failed to serialize: %w", err)
+	}
+
+	logger.Info("Assigning", slog.String("path", path), slog.String("newPath", newPath))
+
+	return os.Remove(path)
+}
+
+func assignIDs(prefix, dir string, format fileFormat) error {
+	defaultYear := time.Now().Year()
+	var unassigned []string
+	yearCounters := map[int]int{}
+
+	// Look for unassigned vulnerabilities, as well as the maximum allocated IDs for every year.
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("failed to access %s: %w", path, err)
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		filename := filepath.Base(path)
+		if isUnassigned(prefix, filename) {
+			unassigned = append(unassigned, path)
+			return nil
+		}
+
+		year, num := extractYearAndNum(prefix, filename, format)
+		if year == 0 || num == 0 {
+			return nil
+		}
+
+		if num > yearCounters[year] {
+			yearCounters[year] = num
+		}
+
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to walk: %w", err)
+	}
+
+	if len(unassigned) == 0 {
+		logger.Info("Nothing to allocate")
+		return nil
+	}
+
+	logger.Info("Assigning IDs using detected maximums", slog.Any("counters", yearCounters))
+	for _, path := range unassigned {
+		if err := assignID(prefix, path, format, yearCounters, defaultYear); err != nil {
+			return fmt.Errorf("failed to assign ID for %s: %w", path, err)
+		}
+	}
+
+	b := make([]byte, conflictMarkerSize)
+	if _, err := rand.Read(b); err != nil {
+		return fmt.Errorf("failed to generate random string: %w", err)
+	}
+
+	return os.WriteFile(filepath.Join(dir, conflictFile), []byte(hex.EncodeToString(b)), 0600)
+}
+
+func readVulnWithFormat(r io.Reader, format fileFormat) (*vulns.Vulnerability, error) {
+	switch format {
+	case fileFormatJSON:
+		return vulns.FromJSON(r)
+	case fileFormatYAML:
+		return vulns.FromYAML(r)
+	default:
+		return nil, fmt.Errorf("unknown file format: %v", format)
+	}
+}
+
+func writeVulnWithFormat(v *osvschema.Vulnerability, w io.Writer, format fileFormat) error {
+	jsonBytes, err := protojson.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	var data []byte
+	switch format {
+	case fileFormatJSON:
+		var buf bytes.Buffer
+		if err := json.Indent(&buf, jsonBytes, "", "  "); err != nil {
+			return err
+		}
+		data = buf.Bytes()
+	case fileFormatYAML:
+		data, err = yaml.JSONToYAML(jsonBytes)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("unknown file format: %v", format)
+	}
+
+	// Ensure the output has a trailing newline to match the behavior of
+	// json.Encoder, which was previously used.
+	if len(data) > 0 && data[len(data)-1] != '\n' {
+		data = append(data, '\n')
+	}
+
+	_, err = w.Write(data)
+
+	return err
+}
---
+++ vulnfeeds/cmd/pypi/main.go
@@ -0,0 +1,235 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package main contains a utility to generate PyPI OSV records.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/osv/external/pypi"
+	"github.com/google/osv/vulnfeeds/conversion"
+	"github.com/google/osv/vulnfeeds/models"
+	"github.com/google/osv/vulnfeeds/triage"
+	"github.com/google/osv/vulnfeeds/utility/logger"
+	"github.com/google/osv/vulnfeeds/vulns"
+)
+
+const (
+	extension = ".yaml"
+)
+
+func loadExisting(vulnsDir string) (map[string]bool, error) {
+	ids := map[string]bool{}
+	err := filepath.Walk(vulnsDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return fmt.Errorf("failed to access %s: %w", path, err)
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		if !strings.HasSuffix(path, extension) {
+			return nil
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return fmt.Errorf("failed to open %s: %w", path, err)
+		}
+		defer f.Close()
+
+		vuln, err := vulns.FromYAML(f)
+		if err != nil {
+			return fmt.Errorf("failed to parse %s: %w", path, err)
+		}
+
+		for _, affected := range vuln.GetAffected() {
+			pkgName := affected.GetPackage().GetName()
+			if pkgName == "" {
+				continue
+			}
+			ids[vuln.GetId()+"/"+pkgName] = true
+			for _, alias := range vuln.GetAliases() {
+				ids[alias+"/"+pkgName] = true
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to walk: %w", err)
+	}
+
+	return ids, nil
+}
+
+func anyUnbounded(v *vulns.Vulnerability) bool {
+	for _, affected := range v.Affected {
+		for _, ranges := range affected.GetRanges() {
+			hasFixed := false
+			hasLastAffected := false
+			for _, event := range ranges.GetEvents() {
+				if event.GetFixed() != "" {
+					hasFixed = true
+				}
+				if event.GetLastAffected() != "" {
+					hasLastAffected = true
+				}
+			}
+			if !hasFixed && !hasLastAffected {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	jsonPath := flag.String("nvd_json", "", "Path to NVD CVE JSON.")
+	pypiLinksPath := flag.String("pypi_links", "", "Path to pypi_links.json.")
+	pypiVersionsPath := flag.String("pypi_versions", "", "Path to pypi_versions.json.")
+	falsePositivesPath := flag.String("false_positives", "", "Path to false positives file.")
+	withoutNotes := flag.Bool("without_notes", false, "Output vulnerabilities without notes only.")
+	excludeUnbounded := flag.Bool("exclude_unbounded", false, "Exclude vulnerabilities with unbounded affected ranges.")
+	outDir := flag.String("out_dir", "", "Path to output results.")
+
+	flag.Parse()
+
+	logger.InitGlobalLogger()
+	defer logger.Close()
+
+	data, err := os.ReadFile(*jsonPath)
+	if err != nil {
+		logger.Fatal("Failed to open file", slog.Any("err", err))
+	}
+	var parsed models.CVEAPIJSON20Schema
+	err = json.Unmarshal(data, &parsed)
+	if err != nil {
+		logger.Fatal("Failed to parse NVD CVE JSON", slog.Any("err", err))
+	}
+
+	falsePositives, err := triage.LoadFalsePositives(*falsePositivesPath)
+	if err != nil {
+		logger.Fatal("Failed to load false positives file", slog.String("path", *falsePositivesPath), slog.Any("err", err))
+	}
+
+	ecosystem := pypi.New(*pypiLinksPath, *pypiVersionsPath)
+	existingIDs, err := loadExisting(*outDir)
+	if err != nil {
+		logger.Fatal("Failed to load existing IDs", slog.Any("err", err))
+	}
+
+	for _, cve := range parsed.Vulnerabilities {
+		if falsePositives.CheckID(string(cve.CVE.ID)) {
+			logger.Info("Skipping as a false positive", slog.String("cve", string(cve.CVE.ID)))
+			continue
+		}
+
+		pkgs := ecosystem.Matches(cve.CVE, falsePositives)
+		if len(pkgs) == 0 {
+			continue
+		}
+
+		for _, pkg := range pkgs {
+			if _, exists := existingIDs[string(cve.CVE.ID)+"/"+pkg]; exists {
+				logger.Info("Skipping match as it already exists", slog.String("cve", string(cve.CVE.ID)), slog.String("package", pkg))
+				continue
+			}
+
+			logger.Info("Matched CVE to package", slog.String("cve", string(cve.CVE.ID)), slog.String("package", pkg))
+			validVersions := ecosystem.Versions(pkg)
+			if validVersions == nil {
+				logger.Info("Package does not have valid versions, skipping", slog.String("package", pkg))
+				continue
+			}
+			logger.Info("Got valid versions", slog.Any("versions", validVersions))
+
+			purl := ecosystem.PackageURL(pkg)
+			metrics := &models.ConversionMetrics{
+				CVEID: cve.CVE.ID,
+			}
+			v := generatePyPIAffected(cve.CVE, pkg, validVersions, purl, metrics)
+			if len(v.Affected) == 0 || len(v.Affected[0].GetRanges()) == 0 {
+				logger.Info("No affected versions detected")
+			}
+
+			if *excludeUnbounded && anyUnbounded(v) {
+				logger.Info("Skipping as we could not find an upperbound version", slog.String("cve", string(cve.CVE.ID)))
+				continue
+			}
+
+			pkgDir := filepath.Join(*outDir, pkg)
+			err = os.MkdirAll(pkgDir, 0755)
+			if err != nil {
+				logger.Fatal("Failed to create dir", slog.Any("err", err))
+			}
+
+			vulnPath := filepath.Join(pkgDir, v.Id+extension)
+			if _, err := os.Stat(vulnPath); err == nil {
+				logger.Info("Skipping as it already exists", slog.String("path", vulnPath))
+				continue
+			}
+
+			if len(metrics.Notes) > 0 && *withoutNotes {
+				logger.Info("Skipping as there are notes associated with it", slog.String("path", vulnPath))
+				continue
+			}
+
+			f, err := os.Create(vulnPath)
+			if err != nil {
+				logger.Fatal("Failed to open for writing", slog.String("path", vulnPath), slog.Any("err", err))
+			}
+			defer f.Close()
+			err = v.ToYAML(f)
+			if err != nil {
+				logger.Panic("Failed to write", slog.String("path", vulnPath), slog.Any("err", err))
+			}
+
+			// If there are notes that require human intervention, write them to the end of the YAML.
+			if len(metrics.Notes) > 0 {
+				notesPath := filepath.Join(pkgDir, v.Id+".notes")
+				_, err = f.WriteString("\n# <Vulnfeeds Notes>\n# " + strings.Join(metrics.Notes, "\n# "))
+				if err != nil {
+					logger.Panic("Failed to write", slog.String("path", notesPath), slog.Any("err", err))
+				}
+			}
+		}
+	}
+}
+
+func generatePyPIAffected(cve models.NVDCVE, pkg string, validVersions []string, purl string, metrics *models.ConversionMetrics) *vulns.Vulnerability {
+	id := "PYSEC-0000-" + cve.ID
+	versions := conversion.ExtractVersionInfo(cve, validVersions, http.DefaultClient, metrics, nil)
+
+	pkgInfo := vulns.PackageInfo{
+		PkgName:     pkg,
+		Ecosystem:   "PyPI",
+		PURL:        purl,
+		VersionInfo: versions,
+	}
+	v := vulns.FromNVDCVE(id, cve)
+	v.AddPkgInfo(pkgInfo)
+
+	return v
+}

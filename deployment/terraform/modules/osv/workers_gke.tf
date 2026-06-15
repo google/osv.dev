5d8fc92e80504d46bf3721000245909bcdf078c4
michaelkedar <michaelkedar@google.com>
2026-06-15 01:11:50 +0000

    chore: tweak test instance worker gke pools

--- deployment/terraform/modules/osv/workers_gke.tf
+++ deployment/terraform/modules/osv/workers_gke.tf
@@ -180,7 +180,7 @@ resource "google_container_node_pool" "worker_pool_temp" {
   }
 
   node_config {
-    machine_type = "n2-highcpu-16"
+    machine_type = "n4-highcpu-2"
 
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
 

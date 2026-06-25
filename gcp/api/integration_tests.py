faa18f5eaaa8913acc61c77acfc3bf44c56db55b
Rex P <rexpan@google.com>
2026-06-25 19:24:02 +1000

    fix: integration test new vuln appeared for ws

--- gcp/api/integration_tests.py
+++ gcp/api/integration_tests.py
@@ -492,8 +492,8 @@ class IntegrationTests(unittest.TestCase,
         timeout=_TIMEOUT)
 
     response_json = response.json()
-    self.assertEqual(2, len(response_json['vulns']))
-    self.assertCountEqual(['GHSA-6fc8-4gx4-v693', 'GHSA-3h5v-q93c-6h6q'],
+    self.assertEqual(3, len(response_json['vulns']))
+    self.assertCountEqual(['GHSA-6fc8-4gx4-v693', 'GHSA-3h5v-q93c-6h6q', 'GHSA-96hv-2xvq-fx4p'],
                           [vuln['id'] for vuln in response_json['vulns']])
 
   def test_query_purl(self):

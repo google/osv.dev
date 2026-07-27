f78abe795ce0e39d8926466aa266d2774a9fd4cf
Michael Kedar <michaelkedar@google.com>
2026-07-27 16:06:22 +1000

    refactor(website): begin migrating Jinja templates to Go (#5694)
    Start migrating many of the website templates from Jinja templates to
    use go's native `html/template` syntax.
    This does most pages except the individual vulnerability page and the
    list page (because those templates are more involved, and were going to
    make the PR too big).
    
    The first commit on this branch is me copying over the original Python
    templates to a new folder - you may want to compare against that commit
    to see the actual changes to the templates

---
+++ go/internal/website/pagedata.go
@@ -0,0 +1,59 @@
+package website
+
+import "html/template"
+
+// BasePageData contains common fields required by the base.html layout template.
+type BasePageData struct {
+	ActiveSection     string
+	DisableTurboCache bool
+}
+
+// EcosystemDisplay holds pre-calculated bubble data for ecosystem vulnerability counts on the home page.
+type EcosystemDisplay struct {
+	Name       string
+	Count      int
+	Radius     float64
+	TooltipTop float64
+}
+
+// HomePageData represents the data context passed to home.html template.
+type HomePageData struct {
+	BasePageData
+
+	Ecosystems []EcosystemDisplay
+}
+
+// NotFoundPageData represents the data context passed to 404.html template.
+type NotFoundPageData struct {
+	BasePageData
+
+	FailedImportVulnID string
+}
+
+// BlogPageData represents the data context passed to blog.html template.
+type BlogPageData struct {
+	BasePageData
+
+	Index template.HTML
+}
+
+// BlogPostPageData represents the data context passed to blog_post.html template.
+type BlogPostPageData struct {
+	BasePageData
+
+	Content template.HTML
+}
+
+// LinterPageData represents the data context passed to linter.html template.
+type LinterPageData struct {
+	BasePageData
+}
+
+// TriagePageData represents the data context passed to triage.html template.
+type TriagePageData struct {
+	BasePageData
+
+	// Columns contains 1-indexed column numbers (e.g., []int{1, 2, 3}) to match
+	// element IDs (e.g. column-1) and URL query parameters (s1, s2, s3) expected by triage.js.
+	Columns []int
+}

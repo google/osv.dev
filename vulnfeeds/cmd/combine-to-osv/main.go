f7c8eb61d57c2a57d46739159c014295c7287b95
Jess Lowe <jesslowe@google.com>
2026-07-29 01:55:28 +0000

    set record for deletion if withdrawn in combine to osv

--- vulnfeeds/cmd/combine-to-osv/main.go
+++ vulnfeeds/cmd/combine-to-osv/main.go
@@ -136,18 +136,21 @@ func readVulnerability(ctx context.Context, client *storage.Client, fullPath str
 }
 
 func combineIntoOSV(cve5 *osvschema.Vulnerability, nvd *osvschema.Vulnerability) *osvschema.Vulnerability {
-	var baseOSV *osvschema.Vulnerability
-	if cve5 != nil && nvd != nil {
-		baseOSV = combineTwoOSVRecords(cve5, nvd)
-	} else if cve5 != nil {
-		baseOSV = cve5
-	} else if nvd != nil {
-		baseOSV = nvd
-	} else {
+	if (cve5 != nil && cve5.Withdrawn != nil) || (nvd != nil && nvd.Withdrawn != nil) {
 		return nil
 	}
 
-	return baseOSV
+	if cve5 != nil && nvd != nil {
+		return combineTwoOSVRecords(cve5, nvd)
+	}
+	if cve5 != nil {
+		return cve5
+	}
+	if nvd != nil {
+		return nvd
+	}
+
+	return nil
 }
 
 func readAndCombineWorker(ctx context.Context, client *storage.Client, workChan <-chan *CVEWorkItem, vulnChan chan<- *osvschema.Vulnerability) {
@@ -367,6 +370,7 @@ func main() {
 // combineTwoOSVRecords takes two osv records and combines them into one
 func combineTwoOSVRecords(cve5 *osvschema.Vulnerability, nvd *osvschema.Vulnerability) *osvschema.Vulnerability {
 	baseOSV := cve5
+
 	if baseOSV.GetDetails() == "" && nvd.GetDetails() != "" {
 		baseOSV.Details = nvd.GetDetails()
 	}

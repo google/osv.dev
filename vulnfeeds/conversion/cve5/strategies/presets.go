c97c6f07895c33784cbed00bb14d71a711a462cf
Jess Lowe <jesslowe@google.com>
2026-09-25 06:07:39 +0000

    add examples to presets.go and remove wp cnas for now

--- vulnfeeds/conversion/cve5/strategies/presets.go
+++ vulnfeeds/conversion/cve5/strategies/presets.go
@@ -2,6 +2,9 @@ package strategies
 
 // Strategy Pipeline Presets for different CNAs
 
+// Default returns the default strategy pipeline for CNAs without a custom preset.
+// Example: test_data/cvelistV5/cves/2025/1xxx/CVE-2025-1110.json (GitLab),
+// test_data/cvelistV5/cves/2026/67xxx/CVE-2026-67185.json (VulnCheck)
 func Default() []VersionStrategy {
 	return []VersionStrategy{
 		&SplitRangeStrategy{},
@@ -15,6 +18,9 @@ func Default() []VersionStrategy {
 	}
 }
 
+// GitHub returns the strategy pipeline for GitHub_M / GitHub advisories.
+// Example: test_data/cvelistV5/cves/2023/45xxx/CVE-2023-45803.json,
+// test_data/cvelistV5/cves/2024/21xxx/CVE-2024-21634.json
 func GitHub() []VersionStrategy {
 	return []VersionStrategy{
 		&SplitRangeStrategy{},
@@ -27,36 +33,11 @@ func GitHub() []VersionStrategy {
 	}
 }
 
-func WPScan() []VersionStrategy {
-	return []VersionStrategy{
-		&ChangesAtStrategy{},
-		&StandardRangeStrategy{},
-		&StringRangeExpressionStrategy{},
-		&CPEVersionStringStrategy{},
-		&ZeroIntroducedSingleVersionStrategy{},
-	}
-}
 
-func Wordfence() []VersionStrategy {
-	return []VersionStrategy{
-		&ChangesAtStrategy{},
-		&StandardRangeStrategy{},
-		&StringRangeExpressionStrategy{},
-		&CPEVersionStringStrategy{},
-		&ZeroIntroducedSingleVersionStrategy{},
-	}
-}
 
-func Patchstack() []VersionStrategy {
-	return []VersionStrategy{
-		&ChangesAtStrategy{},
-		&StandardRangeStrategy{},
-		&StringRangeExpressionStrategy{},
-		&CPEVersionStringStrategy{},
-		&ZeroIntroducedSingleVersionStrategy{},
-	}
-}
-
+// MITRE returns the strategy pipeline for MITRE advisories.
+// Example: test_data/cvelistV5/cves/2021/26xxx/CVE-2021-26917.json,
+// test_data/cve5/CVE-2016-1897.json
 func MITRE() []VersionStrategy {
 	return []VersionStrategy{
 		&SplitRangeStrategy{},
@@ -70,6 +51,9 @@ func MITRE() []VersionStrategy {
 	}
 }
 
+// Linux returns the strategy pipeline for Linux kernel advisories.
+// Example: test_data/cvelistV5/cves/2025/21xxx/CVE-2025-21772.json,
+// test_data/cvelistV5/cves/2025/21xxx/CVE-2025-21631.json
 func Linux() []VersionStrategy {
 	return []VersionStrategy{
 		&InverseAffectedRangesStrategy{},
